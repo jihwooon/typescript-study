b33c3c557a81d0ba28119982ec026fe3bbce6d33
jihwooon <jihwooon@gmail.com>
2023-10-11 16:23:07 +0900

    단일 연결리스트 reverse를 구현하라

--- Data-Structures/src/list/single-linked-list.js
+++ Data-Structures/src/list/single-linked-list.js
@@ -123,6 +123,21 @@ class SinglyLinkedList {
     previousNode.next = removed.next;
     return removed;
   }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
@@ -133,4 +148,6 @@ list.push('1');
 list.set(1, 'change');
 list.indexOf('!');
 list.insert(1, 'first');
-list.remove(1);
+list.reverse();
+
+console.log(list);
