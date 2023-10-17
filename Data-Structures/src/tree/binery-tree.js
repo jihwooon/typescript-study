b7b8ad8198ebe8495ce8ebf125280c6686b038b7
jihwooon <jihwooon@gmail.com>
2023-10-17 15:00:54 +0900

    이진 검색 트리 삽입을 구현하라

--- Data-Structures/src/tree/binery-tree.js
+++ Data-Structures/src/tree/binery-tree.js
@@ -10,12 +10,40 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
-
-console.log(tree);
+tree.insert(10);
+tree.insert(10);
+tree.insert(20);
+tree.insert(21);
+tree.insert(22);
+tree.insert(23);
+const newNode = tree.insert(5);
+console.log(newNode);
