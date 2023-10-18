492d24ced7581659d98aef08fd13a75b28de8f30
jihwooon <jihwooon@gmail.com>
2023-10-18 18:12:47 +0900

    React 컴포넌트에 동적 데이터와 CSS를 추가하라

--- React-Action/ch01/src/components/ExpenseItem.js
+++ React-Action/ch01/src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
+import './ExpenseItem.css'
+
 export const ExpenseItem = () => {
-    return (
-      <div>
-        <h2>Expense Item!</h2>
-        <p>This is also visible!</p>
+  const expenseDate = new Date(2021, 2, 28)
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
-    )
+    </div>
+  )
 }
