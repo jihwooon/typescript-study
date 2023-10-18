8910d70201d3d2b589628abb038ebf7008b40913
jihwooon <jihwooon@gmail.com>
2023-10-18 22:44:18 +0900

    react 컴포지션을 구현하라

--- React-Action/ch01/src/components/ExpenseItem.js
+++ React-Action/ch01/src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import './ExpenseItem.css'
 import { ExpenseDate } from './ExpenseDate'
+import { Card } from './Card'
 
 export const ExpenseItem = ({ date, title, amount }) => {
 
   return (
-    <div className='expense-item'>
-      <div>
-        <ExpenseDate date={date} />
-      </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
