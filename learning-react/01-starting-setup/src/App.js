c8f7c457b7e443a5610fa1180759c39922423768
jihwooon <jihwooon@gmail.com>
2023-11-26 19:37:45 +0900

    리액트 기초 및 실습 컴포넌트를 구현하라

--- learning-react/01-starting-setup/src/App.js
+++ learning-react/01-starting-setup/src/App.js
@@ -1,7 +1,17 @@
-function App() {
+import Expense from "./components/Expense"
+
+const App = () => {
+  const expense = [
+    { title: 'Car Insurance', amount: 788.49, date: new Date(2021, 2, 28) },
+    { title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 8, 13) },
+    { title: 'New Dask(Wooden)', amount: 450, date: new Date(2011, 6, 11) }
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <Expense items={expense} />
     </div>
   );
 }
---
+++ learning-react/01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css'
+
+const ExpenseDate = ({date}) => {
+  const month = date.toLocaleString('en-KR', { month: 'long' });
+  const year = date.getFullYear();
+  const day = date.toLocaleString('en-KR', { day: '2-digit' })
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  )
+}
+
+export default ExpenseDate;
