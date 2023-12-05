604792b97da886a60450b911ffe98ba1fdd0fa5e
jihwooon <jihwooon@gmail.com>
2023-12-06 00:26:47 +0900

    dev-corse 폴더를 삭제하라
    이 폴더 과제를 다른 Repository로 옮겼습니다

--- dev-corse/week2/index.js
+++
@@ -1,9 +0,0 @@
-let server = require('./server');
-let router = require('./router');
-let requestHandler = require('./requestHandler')
-
-let mariadb = require('./databse/connect/mariadb')
-mariadb.connect();
-
-server.start(router.route, requestHandler.handle);
-
