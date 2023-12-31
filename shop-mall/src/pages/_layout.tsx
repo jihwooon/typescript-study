4bf131fe64a5168942cc2f90f53a3c33d8323566
jihwooon <jihwooon@gmail.com>
2024-01-01 03:01:52 +0900

    Global Navigation Bar를 생성하라
    전체 페이지에 적용되는 네이게이션 바를 추가했습니다

--- shop-mall/src/pages/_layout.tsx
+++ shop-mall/src/pages/_layout.tsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 import { getClient } from "../queryClient";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClientProvider } from 'react-query'
+import Gnb from './gnb';
 
 const Layout: React.FC = () => {
   const queryClient = getClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Suspense fallback={'loading...'}>
+        <Gnb />
         <Outlet />
       </Suspense>
       <ReactQueryDevtools initialIsOpen={false} />
