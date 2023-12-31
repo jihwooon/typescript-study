17a64806ca4332b84d38397d895ef5bd33b9834d
jihwooon <jihwooon@gmail.com>
2024-01-01 02:35:26 +0900

    react-query에 cache를 적용하라

--- shop-mall/src/queryClient.ts
+++ shop-mall/src/queryClient.ts
@@ -1,12 +1,20 @@
 import { QueryClient } from 'react-query'
 
-type AnyOBJ = { [key: string]: unknown }
+type AnyOBJ = { [key: string]: any }
 
 export const getClient = (() => {
   let clinet: QueryClient | null = null;
   return () => {
     if (!clinet) clinet = new QueryClient({
-
+      defaultOptions: {
+        queries: {
+          cacheTime: 1000 * 60 * 60 * 24,
+          staleTime: 1000 * 60,
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+        }
+      }
     })
     return clinet;
   }
@@ -17,6 +25,8 @@ const BASE_URL = 'https://fakestoreapi.com'
 export const fetcher = async ({
   method,
   path,
+  params,
+  body,
 }: {
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
   path: string;
@@ -24,7 +34,7 @@ export const fetcher = async ({
   params?: AnyOBJ
 }) => {
   try {
-    const url = `${BASE_URL}${path}`
+    let url = `${BASE_URL}${path}`
     const fetchOptions: RequestInit = {
       method,
       headers: {
@@ -32,6 +42,12 @@ export const fetcher = async ({
         'Access-Control-Allow-Origin': BASE_URL
       }
     }
+    if (params) {
+      const searchParams = new URLSearchParams(params)
+      url += '?' + searchParams.toString();
+    }
+
+    if (body) fetchOptions.body = JSON.stringify(body);
 
     const res = await fetch(url, fetchOptions)
     const json = await res.json();
