e560af0d1f19b52dd27fe0ca196bf1a507a0b8d6
jihwooon <jihwooon@gmail.com>
2024-01-01 23:48:58 +0900

    Recoil를 통한 장바구니 API를 생성하라

--- shop-mall/src/components/products/item.tsx
+++ shop-mall/src/components/products/item.tsx
@@ -1,20 +1,27 @@
 import { Link } from 'react-router-dom'
 import { Product } from '../../graphql/products'
+import { CartItemSelector } from '../../recoils/cart'
+import { useRecoilState } from 'recoil'
 
 const ProductItem = ({
   id,
   imageUrl,
   price,
   title,
-}: Product) => (
+}: Product) => {
+  const [ cartAmount, setCartAmount ] = useRecoilState(CartItemSelector(id))
+  const addToCart = () => setCartAmount(prev => (prev || 0) + 1);
+
+  return (
   <li className="products-item">
     <Link to={`/products/${id}`}>
       <p className="products-item__title">{title}</p>
       <img className="products-item__image" src={imageUrl} />
       <span className="products-item__price">${price}</span>
     </Link>
-    <button className="products-item__add-cart">장바구니 담기</button>
+    <button className="products-item__add-cart" onClick={addToCart}>담기</button>
+    <span>{cartAmount || 0}</span>
   </li>
-)
+)}
 
 export default ProductItem
