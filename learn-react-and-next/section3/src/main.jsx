222ee5d9c1c912ace31f360f5cf304f62b726920
jihwooon <jihwooon@gmail.com>
2024-01-20 17:58:21 +0900

    useReducer를 활용한 A, B 컴포넌트를 추가하라
    - src/exam/A.jsx를 새로 생성했습니다:
      - useState를 활용하여 간단한 카운터 기능을 가진 A 컴포넌트를 추가했습니다.
    
    - src/exam/B.jsx를 새로 생성했습니다:
      - useReducer를 활용하여 간단한 카운터 기능을 가진 B 컴포넌트를 추가했습니다.
      - B 컴포넌트에서 useReducer를 활용하여 DECREASE, INCREASE 액션을 처리하는 로직을 추가했습니다.
    
    - src/main.jsx를 수정했습니다:
      - B 컴포넌트를 import하여 App 대신에 B 컴포넌트를 렌더링하도록 수정했습니다.

--- learn-react-and-next/section3/src/main.jsx
+++ learn-react-and-next/section3/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import B from './exam/B.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // <App />
+  <B/>
 )
