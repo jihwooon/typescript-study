c743d3dcb0d02749d007dc860b16fc3c04c81a51
jihwooon <jihwooon@gmail.com>
2024-01-21 21:19:13 +0900

    데이터 Fetch 기능 확장 및 페이지별 적용하라
    - 검색 결과 조회 및 개별 국가 정보 조회 API 함수를 구현했습니다.
    - 페이지 데이터 Fetch를 통해 국가 코드 및 검색어 기반 데이터 조회 로직을
      추가했습니다.

--- learn-react-and-next/section4/src/api.js
+++ learn-react-and-next/section4/src/api.js
@@ -9,3 +9,23 @@ export const fetchCountries = async () => {
     return [];
   }
 };
+
+export const fetchSearchResults = async (q) => {
+  try {
+    const response = await axios.get(`https://naras-api.vercel.app/search?q=${q}`);
+
+    return response.data;
+  } catch (e) {
+    return [];
+  }
+};
+
+export const fetchCountry = async (code) => {
+  try {
+    const response = await axios.get(`https://naras-api.vercel.app/code/${code}`);
+
+    return response.data;
+  } catch (e) {
+    return null;
+  }
+};
--- learn-react-and-next/section4/src/pages/Country.jsx
+++ learn-react-and-next/section4/src/pages/Country.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchCountry } from '../api';
 
 function Country() {
   const params = useParams();
+  const [country, setCountry] = useState();
+
+  const setInitData = async () => {
+    const data = await fetchCountry(params.code);
+
+    return setCountry(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, [params.code]);
+
   return <div>Country: {params.code}</div>;
 }
 
