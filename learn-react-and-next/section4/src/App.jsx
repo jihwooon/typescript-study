4fadbaf06096d1c05dc357410ea82832ea15a9d2
jihwooon <jihwooon@gmail.com>
2024-01-21 19:07:14 +0900

    React 라우터를 추가하라
    React Router를 추가했습니다.
    
    - react-router-dom 패키지를 설치하고, 버전을 package.json과 package-lock.json에 추가했습니다.
    - App 컴포넌트에 react-router-dom의 Router와 Routes를 사용하여 페이지
      라우팅을 설정하였습니다.
    - main.jsx에서 BrowerRouter를 적용했습니다.
    - App.jsx에 Routes, Route 컴포넌트로 기본 라우팅을 구성했습니다.

--- learn-react-and-next/section4/src/App.jsx
+++ learn-react-and-next/section4/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Country from './pages/Country';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/country" element={<Country />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
