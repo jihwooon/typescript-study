2c2d0216948cd816014ba924b326cbe0f6dabdd9
jihwooon <jihwooon@gmail.com>
2024-01-21 19:21:53 +0900

    Nav 기능 추가 및 ESLint 규칙을 수정하라
    - Link 컴포넌트와 버튼을 사용하여 페이지 간 이동 가능한 기능을
    구현했습니다.
    - useNavi1gate 훅을 사용하여 탐색 기능 구현 함수를 구현했습니다.
    - ESLint 규칙 수정을 하여 object-curly-newline 및 react/button-has-type
      규칙 비활성화했습니다

--- learn-react-and-next/section4/.eslintrc.cjs
+++ learn-react-and-next/section4/.eslintrc.cjs
@@ -48,5 +48,7 @@ module.exports = {
     'key-spacing': ['error', { mode: 'strict' }],
     'arrow-spacing': ['error', { before: true, after: true }],
     "import/no-extraneous-dependencies": "off",
+    "object-curly-newline": "off",
+    "react/button-has-type": "off"
   },
 };
--- learn-react-and-next/section4/src/App.jsx
+++ learn-react-and-next/section4/src/App.jsx
@@ -1,17 +1,32 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import Country from './pages/Country';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Search from './pages/Search';
 
 function App() {
+  const nav = useNavigate();
+
+  const onClick = () => {
+    nav('/search');
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/country" element={<Country />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/country" element={<Country />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/country">Country</Link>
+
+        <button onClick={onClick}>서치페이지로 이동</button>
+      </div>
+    </>
   );
 }
 
