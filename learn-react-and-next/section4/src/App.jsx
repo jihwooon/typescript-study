90d4234a8d2e399ac95287ba7c7ab960716cfaad
jihwooon <jihwooon@gmail.com>
2024-01-21 20:15:17 +0900

    레이아웃 컴포넌트와 스타일을 적용하라
    - Layout 컴포넌트를 생성하여 헤더, 메인 영역에 적용했습니다.
    - 레이아웃 컴포넌트 및 스타일 생성했습니다.
    - 네이게이션 링크 제거했습니다.

--- learn-react-and-next/section4/src/App.jsx
+++ learn-react-and-next/section4/src/App.jsx
@@ -1,32 +1,20 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Country from './pages/Country';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Search from './pages/Search';
+import Layout from './components/Layout';
 
 function App() {
-  const nav = useNavigate();
-
-  const onClick = () => {
-    nav('/search');
-  };
-
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/country/:code" element={<Country />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/country">Country</Link>
-
-        <button onClick={onClick}>서치페이지로 이동</button>
-      </div>
-    </>
+    </Layout>
   );
 }
 
