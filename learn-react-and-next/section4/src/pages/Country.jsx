53988899a3fd594df6c3f1e60ca9433566139018
jihwooon <jihwooon@gmail.com>
2024-01-22 00:04:01 +0900

    국가 상세 페이지 UI 및 데이터 출력 구현하라
    - 국가 정보 데이터를 렌더링 하도록 작업했습니다.
    - 데이터 처리가 안 될 시 Loading 처리 상태를 표시하도록 에러 처리를
      했습니다.
    - CSS 스타일링을 적용했습니다

--- learn-react-and-next/section4/src/pages/Country.jsx
+++ learn-react-and-next/section4/src/pages/Country.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchCountry } from '../api';
+import style from './Country.module.css';
 
 function Country() {
   const params = useParams();
@@ -8,7 +9,6 @@ function Country() {
 
   const setInitData = async () => {
     const data = await fetchCountry(params.code);
-
     return setCountry(data);
   };
 
@@ -16,7 +16,41 @@ function Country() {
     setInitData();
   }, [params.code]);
 
-  return <div>Country: {params.code}</div>;
+  if (!country) {
+    return <div>Loading ....</div>;
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.commonName}>
+          {country.flagEmoji}&nbsp; {country.commonName}
+        </div>
+        <div className={style.officialName}>{country.officialName}</div>
+      </div>
+      <img
+        src={country.flagImg}
+        alt={`${country.commonName}의 국기 이미지입니다.`}
+      />
+      <div className={style.body}>
+        <div>
+          <b>코드 : </b>{country.code}&nbsp;
+        </div>
+        <div>
+          <b>수도 : </b>{country.capital.join(', ')}&nbsp;
+        </div>
+        <div>
+          <b>지역 : </b>&nbsp;{country.region}
+        </div>
+        <div>
+          <b>지도 : </b>&nbsp;
+          <a target="_blank" href={country.googleMapURL} rel="noreferrer">
+            {country.googleMapURL}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Country;
