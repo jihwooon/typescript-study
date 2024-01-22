d2a9c789ca8c74236b4200f3f246236a052982ca
jihwooon <jihwooon@gmail.com>
2024-01-22 14:46:03 +0900

    Vitest 설정 파일 이름 변경하라
    - 테스트 설정 파일의 이름을 setupTests.js에서 vitest.setup.js로
    변경했습니다.
    - vite.config.js에서 설정 파일 경로를 vitest.setup.js로 업데이트를
      했습니다

--- react-testing/vite-starter/vite.config.js
+++ react-testing/vite-starter/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     // this points to the setup file that we created earlier
-    setupFiles: "./setupTests.js",
+    setupFiles: "./vitest.setup.js",
     // you might want to disable the `css: true` line, since we don't have
     // tests that rely on CSS -- and parsing CSS is slow.
     // I'm leaving it in here becasue often people want to parse CSS in tests.
