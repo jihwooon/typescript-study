c3b883517f5ddabba38497fd05a35e83f4c3ede3
jihwooon <jihwooon@gmail.com>
2024-01-30 08:22:17 +0900

    factorial.test.js의 주석 정리 및 sumRange() 재귀 테스트 추가하기
    - factorial.test.js 파일의 주석을 정리하고, sumRange.test.js 파일을 추가했습니다.

--- Data-Structures/src/recursion/factorial.test.js
+++ Data-Structures/src/recursion/factorial.test.js
@@ -3,9 +3,9 @@ const factorial = (num) => ((num === 1) ? 1 : num * factorial(num - 1));
 describe('factorial 함수가 주어지면', () => {
   it('팩토리얼 수로 반환해야 합니다.', () => {
     expect(factorial(1)).toBe(1);
-    expect(factorial(2)).toBe(2); // 2 * 1
-    expect(factorial(3)).toBe(6); // 3 * 2 * 1
-    expect(factorial(4)).toBe(24); // 3 * 2 * 1
-    expect(factorial(5)).toBe(120); // 5 * 4 * 3 * 2 * 1
+    expect(factorial(2)).toBe(2);
+    expect(factorial(3)).toBe(6);
+    expect(factorial(4)).toBe(24);
+    expect(factorial(5)).toBe(120);
   });
 });
