9ddf63cda7e1d6417a2972c6ca78dd1ee2aec2db
jihwooon <jihwooon@gmail.com>
2024-02-01 20:13:33 +0900

    TypeScript 타입 가드와 never 타입 활용 예제 추가
    - pro-typescript/src/ch06/03.ts:
    
     - Bird와 Horse 인터페이스 정의
      - Animal 유니온 타입 정의
      - never 타입을 활용한 타입 가드 함수 assertExhausted 정의
      - 타입 가드를 활용한 객체 분기 처리: moveAnimal 함수

---
+++ pro-typescript/src/ch06/03.ts
@@ -0,0 +1,26 @@
+interface Bird {
+    type: 'bird'
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse'
+    runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+const assertExhausted = (x: never): never => {
+    throw new Error(`Unexpected object: ${x}`)
+}
+
+const moveAnimal = (animal: Animal) => {
+    switch (animal.type) {
+        case 'bird': return animal.flyingSpeed
+        case 'horse': return animal.runningSpeed
+        default: assertExhausted(animal)
+    }
+}
+
+console.log(moveAnimal({ 'type': 'bird', flyingSpeed: 200 }))
+console.log(moveAnimal({ 'type': 'horse', runningSpeed: 100 }))
