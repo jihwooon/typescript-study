6adfa84f6f22d5d995df7fa460beca1c11f1b40e
jihwooon <jihwooon@gmail.com>
2024-02-05 16:32:25 +0900

    TimerChallenge 컴포넌트에 타이머, 결과 모달, 상태 관리 기능 추가
    - ResultModal 컴포넌트 생성:
      - 결과 표시를 위한 모달 컴포넌트
      - TimerChallenge 컴포넌트 수정:
    - useState와 useRef 훅을 사용하여 타이머 상태, 결과 모달, 시간 초과 상태 관리
      - handleStart 함수에서 타이머 설정 및 결과 모달 표시
      - handleStop 함수에서 타이머 정지
      - 버튼 텍스트와 타이머 상태 표시를 조건부 렌더링으로 동적 변경
      - 시간 초과 시 "You lost!" 메시지 표시

--- react-in-action/ch08/src/components/TimerChallenge.jsx
+++ react-in-action/ch08/src/components/TimerChallenge.jsx
@@ -1,18 +1,39 @@
+import ResultModal from "./ResultModal";
+import { useState, useRef } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+    const timer = useRef();
+    const dialog = useRef();
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timeExpired, setTimeExpired] = useState(false);
+
+    const handleStart = () => {
+        timer.current = setTimeout(() => { setTimeExpired(true) }, targetTime * 1000);
+        dialog.current.showModal();
+        setTimerStarted(true)
+    };
+
+    const handleStop = () => clearTimeout(timer.current);
+
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <p>
-                <button>
-                    Start Challenge
-                </button>
-            </p>
-            <p className="">
-                Time is running ... /Timer inactive
-            </p>
-        </section>
+        <>
+            <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+            <section className="challenge">
+                <h2>{title}</h2>
+                {timeExpired && <p>You lost!</p>}
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? 'Stop' : 'Start'} Challenge
+                    </button>
+                </p>
+                <p className={timerStarted ? 'active' : undefined}>
+                    {timerStarted ? 'Time is running...' : 'Timer inactive'}
+                </p>
+            </section>
+        </>
     )
 }
