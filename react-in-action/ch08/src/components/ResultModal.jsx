e863dbb105de6c9b168c57dedc9ccabccc6bcd0b
jihwooon <jihwooon@gmail.com>
2024-02-05 17:42:21 +0900

    ResultModal을 Portal로 렌더링하여 DOM 구조 개선
    - createPortal을 사용하여 ResultModal 컴포넌트를 루트 DOM 밖에 렌더링
    - document.getElementById('modal')에 모달 렌더링

--- react-in-action/ch08/src/components/ResultModal.jsx
+++ react-in-action/ch08/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom'
 
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime, remainingTime, onReset }, ref) {
@@ -16,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime, remain
         }
     });
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" open>
             {userLost && <h2>You {result}</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -25,7 +26,8 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime, remain
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 })
 
