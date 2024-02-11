e06db5a6292955699ed0163913b02f065ed3126c
jihwooon <jihwooon@gmail.com>
2024-02-11 18:54:23 +0900

    CPU 및 Disk 서비스에 PowerModule 의존성 주입 및 서비스 간 통신 구현
    - src/cpu/cpu.module.ts와 src/disk/disk.module.ts 파일에 PowerModule을 imports 배열에 추가
    - src/cpu/cpu.service.ts와 src/disk/disk.service.ts 파일에:
      - PowerService를 생성자 매개변수로 주입
      - 각 서비스의 메서드에서 PowerService의 supplyPower 메서드를 호출하여 전력 공급 요청

--- nest-in-action/ch05/src/cpu/cpu.service.ts
+++ nest-in-action/ch05/src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { DiskService } from 'src/disk/disk.service';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService { }
+export class CpuService {
+    constructor(private powerService: PowerService) { }
+    
+    compute(a: number, b:number) {
+        this.powerService.supplyPower(10)
+        return a + b;
+    }
+}
