a3c487362b01f620d4bef16790352e62919fda47
jihwooon <jihwooon@gmail.com>
2024-02-14 17:52:24 +0900

    리액트 컴포넌트 개선 및 스타일 적용
    - App.css 파일에:
      - 배경색 검정색(#000000)으로 변경
      - h2 스타일 추가: 빨간색(#ff0000)
    - App.js 파일에:
      - MyHeader 및 MyFooter 컴포넌트 추가 및 사용
      - h2 스타일을 객체 리터럴로 정의하여 적용
      - 랜덤 숫자 생성 및 짝수/홀수 표시 추가
      - 함수형 컴포넌트 func 추가
    - MyFooter.js 및 MyHeader.js 파일 생성:
      - 각각 간단한 컴포넌트 구현 (h2 태그 포함)

--- learn-react/ch01/src/App.js
+++ learn-react/ch01/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import MyFooter from './MyFooter';
+import MyHeader from './MyHeader';
 
 function App() {
+  const style = {
+    h2: {
+      color: "red"
+    }
+  }
+
+  const number = Math.round(Math.random() * 99);
+  console.log(number);
+
+  const func = () => "func"
+
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
-        <h2>안녕 리액트</h2>
+        <h2 style={style.h2}>안녕 리액트{func()}</h2>
+        <b>{number % 2 === 0 ? "짝수" : "홀수"}</b>
       </header>
+      <MyFooter />
     </div>
   );
 }
---
+++ learn-react/ch01/src/MyHeader.js
@@ -0,0 +1,9 @@
+const MyHeader = () => {
+    return (
+        <div>
+         <h2>MyHeadr</h2>
+        </div>
+    )
+}
+
+export default MyHeader;
