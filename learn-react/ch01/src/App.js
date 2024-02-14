2f0da80adb027264ef71da05e8ca02523a873e97
jihwooon <jihwooon@gmail.com>
2024-02-14 18:41:44 +0900

    컨테이너 컴포넌트 추가 및 카운터 개선
    - App.js 파일:
      - Container 컴포넌트를 사용하여 스타일 적용된 영역 생성
      - 카운터 컴포넌트에 props 전달 (a, b, c, initialValue)
    - Container.js 파일:
      - 스타일 적용된 컨테이너 컴포넌트 구현
    - Counter.js 파일:
      - props initialValue 받아서 초기 카운터 값 설정
      - OddEvenResult 컴포넌트 추가하여 짝수/홀수 표시
      - 기본값 설정 (defaultProps)

--- learn-react/ch01/src/App.js
+++ learn-react/ch01/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Counter from './Counter';
+import Container from './Container';
 
 function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    initialValue: 5
+  }
+
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <Container>
+      <div className="App">
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
