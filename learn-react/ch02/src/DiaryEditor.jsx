ae8fcc7157310ea8b1351ea3abb2b8a2731cba52
jihwooon <jihwooon@gmail.com>
2024-02-15 14:40:56 +0900

    DiaryEitor.jsx 파일에 Ref를 추가하라
    - Refs: authorInput과 contentInput이라는 두 개의 ref가 각각 저자와 내용 입력 필드에 추가되었습니다. 이 ref는 코드에서 나중에 입력 요소에 직접 접근하는 데 사용될 수 있습니다.
    - 입력 유효성 검사: handleSubmit 함수는 이제 기본적인 유효성 검사를 수행합니다. 저자 이름이 비어 있는지, 내용 길이가 5자 미만인지 확인합니다. 두 조건 중 하나라도 true라면 해당 입력 필드에 포커스를 맞춘 후 제출을 방지합니다.

--- learn-react/ch02/src/DiaryEditor.jsx
+++ learn-react/ch02/src/DiaryEditor.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,6 +18,16 @@ const DiaryEditor = () => {
   }
 
   const handleSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return
+    }
+
     alert("저장 성공")
   }
 
@@ -23,6 +36,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -30,6 +44,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
