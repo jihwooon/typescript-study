2691347288bb18d9fdda198785178e30545bd0c8
jihwooon <jihwooon@gmail.com>
2024-02-16 18:48:42 +0900

    상태 관리 개선 및 댓글 기능 추가
    * `useReducer` Hook을 사용하여 데이터 상태 관리 개선
    * 댓글 관련 액션 타입 정의 (`CREATE`, `REMOVE`, `EDIT`)
    * `onCreate`, `onRemove`, `onEdit` 함수 개선하여 액션 디스패치
    * 댓글 목록 컴포넌트 (`DiaryList`) 추가 및 데이터 표시
    * 일기 분석 로직 개선 (감정 점수 기준 변경, 소수점 표시)
    * 불필요한 컴포넌트 (`OptimizedTest`) 제거

--- learn-react/ch02/src/App.jsx
+++ learn-react/ch02/src/App.jsx
@@ -1,83 +1,109 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import './App.css'
-import DiaryEditor from './DiaryEditor'
-import DiaryList from './DiaryList'
-import LifeCycle from './Lifecycle'
-import OptimizedTest from './OptimizeTest'
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef
+} from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
 
-function App() {
-  const [data, setData] = useState([])
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+            ...it,
+            content: action.newContent
+          }
+          : it
+      );
+    }
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [data, dispatch] = useReducer(reducer, []);
   const dataId = useRef(0);
-
   const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json())
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
     const initData = res.slice(0, 20).map((it) => {
       return {
         author: it.email,
         content: it.body,
-        emotion: Math.floor(Math.random * 5) + 1,
-        create_date: new Date().getTime(),
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
         id: dataId.current++
-      }
-    })
+      };
+    });
 
-    setData(initData)
-  }
+    dispatch({ type: "INIT", data: initData });
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime()
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current
-    }
+    dispatch({
+      type: "CREATE",
+      data: { author, content, emotion, id: dataId.current }
+    });
     dataId.current += 1;
-    setData((data) => [newItem, ...data])
-  }, [])
+  }, []);
 
-  const onRemove = (targeId) => {
-    const newDiaryList = data.filter((it) => it.id !== targeId)
-    setData(newDiaryList)
-  }
+  const onRemove = useCallback((targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  }, []);
 
-  const onEdit = (targetId, newContent) => {
-    setData(
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it
-      )
-    );
-  }
+  const onEdit = useCallback((targetId, newContent) => {
+    dispatch({
+      type: "EDIT",
+      targetId,
+      newContent
+    });
+  }, []);
 
-  const getDiaryAnalysis = useMemo(() => {
-    const goodCount = data.filter((it) => it.email > 3).length;
+  const memoizedDiaryAnalysis = useMemo(() => {
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
-    const goodRatio = (goodCount / data.length) * 100;
+    const goodRatio = (goodCount / data.length) * 100.0;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
 
-    return { goodCount, badCount, goodRatio }
-  }, [data.length]
-  );
-
-  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  const { goodCount, badCount, goodRatio } = memoizedDiaryAnalysis;
 
   return (
-    <div className='App'>
-      <OptimizedTest />
-      <h1>Hello World!</h1>
-      <LifeCycle />
+    <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <div>전체 일기: {data.length}</div>
-      <div>기분 좋은 일기 개수: {goodCount}</div>
-      <div>기분 나쁜 일기: {badCount}</div>
-      <div>기분 나쁜 일기: {goodRatio}</div>
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}%</div>
       <DiaryList onRemove={onRemove} onEdit={onEdit} dummayList={data} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
