74841d63ac3fda22ce842d9be2e7a0ed0666cc81
jihwooon <jihwooon@gmail.com>
2024-02-21 18:54:17 +0900

    에러 처리 개선, 코드 로깅 추가
    - findUser 메서드에서 유저 데이터 조회 실패 시 NotFoundException 발생
    - 코드 가독성 향상을 위한 로그 메시지 추가
    - update 및 remove 메서드에서 유저 데이터 조회 실패 시 NotFoundException 발생

--- nest-in-action/my-car-value/src/users/users.controller.ts
+++ nest-in-action/my-car-value/src/users/users.controller.ts
@@ -7,9 +7,10 @@ import {
   Query,
   Delete,
   Patch,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { UpdateUserDto } from './dtos/update-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -22,8 +23,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    console.log(user);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
--- nest-in-action/my-car-value/src/users/users.service.ts
+++ nest-in-action/my-car-value/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -24,7 +24,7 @@ export class UsersService {
   async update(id: number, attrt: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     Object.assign(user, attrt);
 
@@ -34,7 +34,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
   }
