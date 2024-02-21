eeb8bcb004b501e54c75c2a0707890c3dcb2dcfc
jihwooon <jihwooon@gmail.com>
2024-02-21 18:14:34 +0900

    사용자 서비스: update 메서드 추가 및 코드 개선
    update 기능과 findOne 기능을 재활용하였습니t다

--- nest-in-action/my-car-value/src/users/users.service.ts
+++ nest-in-action/my-car-value/src/users/users.service.ts
@@ -20,4 +20,22 @@ export class UsersService {
   find(email: string) {
     return this.repo.find({ where: { email } });
   }
+
+  async update(id: number, attrt: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrt);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
