04c58bb057cf1f7337f8ef45fda10ac48605f350
jihwooon <jihwooon@gmail.com>
2024-02-21 18:27:26 +0900

    사용자 조회 기능 추가
    - findUser(@Param('id') id: string) 메서드 추가: ID로 사용자 정보 조회
    - findAllUser(@Query('email') email: string) 메서드 추가: 이메일로 사용자 정보 조회

--- nest-in-action/my-car-value/src/users/users.controller.ts
+++ nest-in-action/my-car-value/src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) { }
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
