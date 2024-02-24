e90096256ea3648d6b14bc90f200d364ad563f11
jihwooon <jihwooon@gmail.com>
2024-02-25 00:00:17 +0900

    직렬화 데코레이터 도입하여 코드 간결화
    주요 변경 사항
    - 직렬화 데코레이터 도입: nest-in-action/my-car-value/src/interceptors/serialize.interceptor.ts 파일에 Serialize 데코레이터 함수 추가
    - 사용자 컨트롤러 개선: nest-in-action/my-car-value/src/users/users.controller.ts 파일에 @Serialize 데코레이터 사용하여 코드 간결화
    
    이유
    - Serialize 데코레이터를 도입하여 코드의 가독성과 유지보수성을 향상시켰습니다.
    - 이제 컨트롤러에서 직렬화 로직을 별도로 설정할 필요 없이 간편하게 사용할 수 있습니다.

--- nest-in-action/my-car-value/src/interceptors/serialize.interceptor.ts
+++ nest-in-action/my-car-value/src/interceptors/serialize.interceptor.ts
@@ -3,10 +3,15 @@ import {
   ExecutionContext,
   CallHandler,
   Injectable,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
+
+export const Serialize = (dto: any) => {
+  return UseInterceptors(new SerializeInterceptor(dto));
+};
 
 @Injectable()
 export class SerializeInterceptor implements NestInterceptor {
