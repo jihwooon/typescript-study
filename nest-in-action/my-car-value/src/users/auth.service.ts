12f2f207b3280d409bd6a5349a9e4018176e363d
jihwooon <jihwooon@gmail.com>
2024-02-25 20:37:40 +0900

    사용자 로그인 기능 구현
    주요 변경 사항
    - 사용자 로그인 기능 구현
      - nest-in-action/my-car-value/src/users/auth.service.ts 파일에 signin 메서드 추가
      - signin 메서드에서 이메일 존재 확인, 암호 일치 여부 검사 로직 구현
      - nest-in-action/my-car-value/src/users/users.controller.ts 파일에 signin 라우트 추가하여 AuthService 의 signin 메서드 사용
    이유
    - 사용자 로그인 기능을 구현하여 시스템 접근을 가능하게 했습니다.
    - 이메일 존재 확인과 암호 일치 여부 검사를 통해 보안을 강화했습니다.

--- nest-in-action/my-car-value/src/users/auth.service.ts
+++ nest-in-action/my-car-value/src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -20,6 +24,23 @@ export class AuthService {
     const hashedPassword = `${salt}.${hash.toString('hex')}`;
 
     const user = await this.userService.create(email, hashedPassword);
+
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.userService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scryptAsync(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+    }
 
     return user;
   }
