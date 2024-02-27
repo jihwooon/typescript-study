64b1281bf36183b697c967f2894408075c5c97f8
jihwooon <jihwooon@gmail.com>
2024-02-27 14:23:15 +0900

    AuthService 테스트 확장 - 로그인 실패 시 에러 처리
    * AuthService 테스트 스펙 파일 확장 (users/auth.service.spec.ts)
    * signin 메서드 테스트 추가: 존재하지 않는 이메일로 로그인 시 NotFoundException 발생 확인
    
    **관련 변경 사항:**
    * UsersService 주석 추가 (테스트 케이스 작성 용)
    
    **참고:**
    * UsersService 변경은 테스트 케이스를 위한 주석 추가이며 실제 로직 변경 없음

--- nest-in-action/my-car-value/src/users/auth.service.spec.ts
+++ nest-in-action/my-car-value/src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -47,6 +47,12 @@ describe('AuthService', () => {
 
     expect(service.signup('asdf@gmail.con', 'asdf')).rejects.toThrow(
       BadRequestException,
+    );
+  });
+
+  it('throw if signin is called with an unsed email', async () => {
+    expect(service.signin('asdf@gmail.com', 'asdf')).rejects.toThrow(
+      NotFoundException,
     );
   });
 });
