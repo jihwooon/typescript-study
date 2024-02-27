f86571289609d66fc9f72a1e09f9a7fadb302d5a
jihwooon <jihwooon@gmail.com>
2024-02-27 13:54:46 +0900

    AuthService 테스트 확장 - 이메일 중복 시 에러 처리
    설명:
    AuthService 테스트 스펙 파일 (users/auth.service.spec.ts)을 확장했습니다.
    
    테스트 케이스 추가:
    signup 메서드 테스트: 이미 사용 중인 이메일로 가입 시 에러 발생 확인

--- nest-in-action/my-car-value/src/users/auth.service.spec.ts
+++ nest-in-action/my-car-value/src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -37,5 +39,14 @@ describe('AuthService', () => {
 
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+    expect(service.signup('asdf@gmail.con', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
