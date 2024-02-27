79a9375abe34ba51c10085888d00bd88ebdee7b3
jihwooon <jihwooon@gmail.com>
2024-02-27 21:02:25 +0900

    테스트 데이터베이스 추가 및 환경변수 활용
    테스트 실행 시 별도의 데이터베이스 파일 (test.sqlite)을 사용하도록 변경했습니다.

--- nest-in-action/my-car-value/src/app.module.ts
+++ nest-in-action/my-car-value/src/app.module.ts
@@ -13,7 +13,7 @@ import cookieSession from 'cookie-session';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
       entities: [User, Report],
       synchronize: true,
     }),
