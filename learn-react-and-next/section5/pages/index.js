32968c3c93bd891506ee4963f5c2a98a38d0701a
jihwooon <jihwooon@gmail.com>
2024-03-19 09:25:21 +0900

    홈 페이지 기능 추가 및 라우팅 활성화
    이 커밋은 리액트 프로젝트의 홈 페이지에 검색 페이지로 이동하는 기능과 동적 라우팅을 활용한 국가 페이지 링크를 추가했습니다.
    
    - 홈 페이지에 'Search 페이지로 이동' 버튼 추가 (Next.js router 이용)
    - Link 컴포넌트를 활용하여 검색 페이지로의 링크 생성 (두 가지 방법 모두 포함)
    - 국가 코드 KOR을 활용하여 국가 페이지 링크 생성 (동적 라우팅)

--- learn-react-and-next/section5/pages/index.js
+++ learn-react-and-next/section5/pages/index.js
@@ -1,7 +1,31 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  return (<div>Home Page</div>);
+  const code = "KOR";
+
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push('/search')
+  }
+
+  return (
+    <>
+      <div>Home Page
+        <div>
+          <button onClick={onClick}>Search 페이지로 이동</button>
+        </div>
+      </div>
+      <div>
+        <Link href={"/search"}>Search Page 이동</Link>
+      </div>
+      <div>
+        <Link href={{
+          pathname: "/country/[code]",
+          query: { code: code},
+        }}>{code} 페이지 이동</Link>
+      </div>
+    </>
+  );
 }
