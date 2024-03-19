40fca312129075af9007665abcb4b10a03e2b95e
jihwooon <jihwooon@gmail.com>
2024-03-19 10:11:27 +0900

    서브 레이아웃 적용 및 footer 추가
    - SubLayout 컴포넌트 생성:
      - 콘텐츠 영역과 푸터 영역으로 구성
      - 푸터에는 저작권 표시 (@jihwan)
    - _app.js 수정:
      - Component.Layout 속성을 통해 페이지별 서브 레이아웃 적용
      - 기본 레이아웃 설정 (EmptyLayout)
    - country/[...code].js 및 search/index.js 수정:
      - SubLayout 컴포넌트 적용

--- learn-react-and-next/section5/pages/_app.js
+++ learn-react-and-next/section5/pages/_app.js
@@ -2,10 +2,15 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+
+  const EmptyLayout = ({children})=><>{children}</>
+const SubLayout = Component.Layout || EmptyLayout;
+
   return (
     <Layout>
-      <header>NARAS</header>
-      <Component {...pageProps} />
+      <SubLayout>
+        <Component {...pageProps} />
+      </SubLayout>
     </Layout>
   );
 }
