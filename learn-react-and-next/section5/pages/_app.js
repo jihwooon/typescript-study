46cd7fdb23166f2c9fb38b44d3a9061399770dee
jihwooon <jihwooon@gmail.com>
2024-03-19 09:39:24 +0900

    레이아웃 및 글로벌 스타일 적용
    - Layout 컴포넌트 생성:
      - 헤더 영역 고정
      - 메인 콘텐츠 영역 스타일링
    - _app.js에서 Layout 컴포넌트 적용:
      - 헤더 부분에 "NARAS " 타이틀 표시
    - globals.css 파일 수정:
      - 불필요한 스타일 제거
      - 기본 배경색 설정 (rgb(245, 245, 245))

--- learn-react-and-next/section5/pages/_app.js
+++ learn-react-and-next/section5/pages/_app.js
@@ -1,5 +1,11 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <header>NARAS</header>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
