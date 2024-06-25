c6c55289bbee9c280a30b9c4b8778d8ade02b545
jihwooon <jihwooon@gmail.com>
2024-06-25 11:15:37 +0900

    feat: 초기 Board 데이터 및 관련 타입 추가
    - boardSlice.ts에 초기 board 데이터 추가
    - IBoardItem, IListItem 인터페이스 추가
    - 기존 ITask 인터페이스에 lists 속성 추가

--- programmers/react-task-app/src/store/slices/boardSlice.ts
+++ programmers/react-task-app/src/store/slices/boardSlice.ts
@@ -1,20 +1,57 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {IBoardItem} from "../../types";
+import { createSlice } from "@reduxjs/toolkit";
+import { IBoardItem } from "../../types";
 
 type boardState = {
-    modalActive: boolean
-    boardArray: IBoardItem[]
-}
+  modalActive: boolean;
+  boardArray: IBoardItem[];
+};
 
 const initialState: boardState = {
-    modalActive: false,
-    boardArray: []
-}
+  modalActive: false,
+  boardArray: [
+    {
+      boardId: "board-0",
+      boardName: "첫 번째 게시물",
+      lists: [
+        {
+          listId: "list-0",
+          listName: "List 1",
+          tasks: [
+            {
+              taskId: "task-0",
+              taskName: "Task 1",
+              taskDescription: "Description",
+              taskOwner: "John",
+            },
+            {
+              taskId: "task-1",
+              taskName: "Task 2",
+              taskDescription: "Description",
+              taskOwner: "John",
+            },
+          ],
+        },
+        {
+          listId: "list-1",
+          listName: "List 2",
+          tasks: [
+            {
+              taskId: "task-3",
+              taskName: "Task 3",
+              taskDescription: "Description",
+              taskOwner: "John",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 const boardSlice = createSlice({
-    name: 'board',
-    initialState: initialState,
-    reducers: {}
+  name: "board",
+  initialState: initialState,
+  reducers: {},
 });
 
 export const boardReducer = boardSlice.reducer;
--- programmers/react-task-app/src/types/index.ts
+++ programmers/react-task-app/src/types/index.ts
@@ -1,18 +1,25 @@
 export interface ITask {
-    taskId: string;
-    taskName: string,
-    taskDescription: string,
-    taskOwner: string
+  taskId: string;
+  taskName: string;
+  taskDescription: string;
+  taskOwner: string;
 }
 
 export interface ILogItem {
-    logId: string;
-    logAuthor: string;
-    logMessage: string;
-    logTimestamp: string;
+  logId: string;
+  logAuthor: string;
+  logMessage: string;
+  logTimestamp: string;
+}
+
+export interface IListItem {
+  listId: string;
+  listName: string;
+  tasks: ITask[];
 }
 
 export interface IBoardItem {
-    boardId: string;
-    boardName: string;
+  boardId: string;
+  boardName: string;
+  lists: IListItem[];
 }
