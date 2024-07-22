185b330e30c3df70a885b18ddc27a32ccfe63431
jihwooon <jihwooon@gmail.com>
2024-07-22 14:10:32 +0900

    Player 컴포넌트 플레이어 이름 변경 기능 구현
    - playerName 상태 추가 및 초기값 설정
    - 입력 필드 값 변경 시 playerName 상태 업데이트하는 handleChane 함수 추가
    - editablePlayerName 변수를 통해 조건부 렌더링 로직 개선
    - 입력 필드에 value 속성을 추가하여 playerName 상태와 동기화

--- react-in-action/ch04-tic-tac-toe-starting-project/src/components/Player.jsx
+++ react-in-action/ch04-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     return setIsEditing((editing) => !editing); // 상태 변경을 함수로 해야 한다.
   };
 
-  const playerName = <span className="player-name">{name}</span>;
-  const inputText = <input type="text" required value={name} />;
+  const handleChane = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  const editablePlayerName = <span className="player-name">{playerName}</span>;
+  const inputText = (
+    <input type="text" required value={playerName} onChange={handleChane} />
+  );
 
   return (
     <li>
       <span className="player-info">
-        {isEditing ? inputText : playerName}
+        {isEditing ? inputText : editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
