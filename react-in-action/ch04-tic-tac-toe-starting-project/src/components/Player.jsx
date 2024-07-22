1df1812aff06bed6fc68a5617193c8f83cb2add6
jihwooon <jihwooon@gmail.com>
2024-07-22 13:44:35 +0900

    플레이어 이름 수정 기능 추가
    - Player 컴포넌트에 isEditing 상태 추가하여 수정 모드 관리
    - Edit 버튼 클릭 시 입력 필드 활성화 및 이름 변경 가능
    - 입력 필드 값 유효성 검사 (required 속성) 추가

--- react-in-action/ch04-tic-tac-toe-starting-project/src/components/Player.jsx
+++ react-in-action/ch04-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    return setIsEditing(true);
+  };
+
+  const playerName = <span className="player-name">{name}</span>;
+  const inputText = <input type="text" required />;
+
   return (
     <li>
       <span className="player-info">
-        <span className="player-name">{name}</span>
+        {isEditing ? inputText : playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
