379d5476b9f53dc311fa6e7466c07fea4022e65a
jihwooon <jihwooon@gmail.com>
2024-07-26 10:13:13 +0900

    공통 Empty 컴포넌트 추가 및 장바구니 빈 상태 처리
    - 새로운 공통 Empty 컴포넌트를 생성하여 빈 상태 표시를 위한 재사용 가능한 솔루션 제공
    - BooksEmpty 컴포넌트를 리팩터링하여 Empty 컴포넌트 사용
    - 장바구니 페이지에 Empty 컴포넌트 적용하여 장바구니가 비어 있을 때 알림 메시지 표시

--- bookstore/frontend/src/pages/Cart.tsx
+++ bookstore/frontend/src/pages/Cart.tsx
@@ -3,9 +3,11 @@ import Title from "../components/common/Title";
 import CartItem from "../components/cart/CartItem";
 import { useCart } from "../hooks/useCart";
 import { useState } from "react";
+import Empty from "../components/common/Empty";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Cart = () => {
-  const { carts, deleteCartItem } = useCart();
+  const { carts, deleteCartItem, isEmpty } = useCart();
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
 
   const handleCheckItem = (id: number) => {
@@ -25,17 +27,28 @@ const Cart = () => {
     <>
       <Title size="large">장바구니</Title>
       <CartStyle>
-        <div className="content">
-          {carts.map((item) => (
-            <CartItem
-              key={item.id}
-              cart={item}
-              checkedItems={checkedItems}
-              onCheck={handleCheckItem}
-              onDelete={handleItemDelete}
-            />
-          ))}
-        </div>
+        {!isEmpty && (
+          <>
+            <div className="content">
+              {carts.map((item) => (
+                <CartItem
+                  key={item.id}
+                  cart={item}
+                  checkedItems={checkedItems}
+                  onCheck={handleCheckItem}
+                  onDelete={handleItemDelete}
+                />
+              ))}
+            </div>
+          </>
+        )}
+        {isEmpty && (
+          <Empty
+            title="장바구니가 비워있습니다"
+            icon={<FaShoppingCart />}
+            description={<>장바구니를 채워보세요</>}
+          ></Empty>
+        )}
         <div>summary</div>
       </CartStyle>
     </>
