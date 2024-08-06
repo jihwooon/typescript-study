fb6a5e2c8ec251b76d3741fa59839492ee86fe3a
jihwooon <jihwooon@gmail.com>
2024-08-06 22:08:38 +0900

    로그인 전 메인 페이지 디자인 개선 및 스타일 수정
    - globals.css에서 다크 모드 관련 설정 주석 처리
    - 메인 페이지 레이아웃 구조 변경 및 스타일 개선
    - 버튼 디자인 및 레이아웃 조정
    - 반응형 디자인을 위한 미디어 쿼리 제거
    - 전체 컨테이너 스타일 추가로 앱 레이아웃 일관성 확보
    
    이러한 변경으로 사용자에게 더 깔끔하고 현대적인 UI를 제공하며,
    로그인 및 회원가입 버튼의 가시성을 높여 사용자 경험을 개선합니다.
    또한 앱 전체의 일관된 레이아웃을 위한 기반을 마련합니다.

--- z-com/src/app/page.tsx
+++ z-com/src/app/page.tsx
@@ -5,17 +5,24 @@ import zLogo from "../../public/zlogo.png"
 
 export default function Home() {
   return (
-      <div className={styles.main}>
+      <>
           <div className={styles.left}>
               <Image src={zLogo} alt="logo"/>
           </div>
           <div className={styles.right}>
               <h1>지금 일어나고 있는 일</h1>
-              <h3>지금 가입하세요</h3>
-              <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
+              <h2>지금 가입하세요</h2>
+              <Link href="/i/flow/signup" className={styles.signup}>
+                  <button>
+                  계정 만들기
+              </button>
+              </Link>
               <h3>이미 트위터에 가입하셨나요?</h3>
-              <Link href="/login" className={styles.signup}>로그인</Link>
+              <Link href="/login" className={styles.login}><button>
+                 로그인
+              </button>
+              </Link>
           </div>
-      </div>
+      </>
   );
 }
