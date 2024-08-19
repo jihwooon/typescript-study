d9e28291079ca5c09d8a02f91f2d93c9f90f0fc6
jihwooon <jihwooon@gmail.com>
2024-08-20 01:22:02 +0900

    App 컴포넌트 개선 및 네비게이션 추가
    이 커밋은 Next.js의 _app.tsx 파일을 수정하여 전역 레이아웃과 네비게이션을 추가합니다. 주요 변경 사항은 다음과 같습니다:
    
    1. 전역 헤더 추가
      - 모든 페이지에 공통적으로 적용되는 헤더 구현
    
    2. 네비게이션 링크 추가
      - Link 컴포넌트를 사용하여 주요 페이지로의 네비게이션 링크 구현
      - 인덱스, 검색, 도서 상세 페이지로의 링크 제공
    
    3. 프로그래매틱 라우팅 예제 추가
      - useRouter 훅을 사용한 버튼 클릭 시 페이지 이동 기능 구현
    
    4. 레이아웃 구조화
      - 헤더와 메인 컨텐츠를 구분하여 레이아웃 구조화
    
    이러한 변경으로 다음과 같은 이점이 있습니다:
    - 일관된 사용자 경험을 위한 전역 네비게이션 제공
    - 다양한 라우팅 방식 (Link 컴포넌트, 프로그래매틱 라우팅) 예시 제공
    - 모든 페이지에 공통 레이아웃 적용 용이성 증대

--- onebit-next/section02/src/pages/_app.tsx
+++ onebit-next/section02/src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    const router = useRouter();
+
+    const onClickButton = () => {
+        router.push('/test')
+    };
+
+  return (
+  <>
+      <header>
+          <Link href={"/"}>index</Link>
+          &nbsp;
+          <Link href={"/search"}>index</Link>
+          &nbsp;
+          <Link href={"/book/1"}>index</Link>
+          <div>
+              <button onClick={onClickButton}>test</button>
+          </div>
+      </header>
+    <Component {...pageProps} />
+  </>
+  );
 }
+
