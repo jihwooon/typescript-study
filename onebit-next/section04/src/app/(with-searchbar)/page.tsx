9b38e252894cd6208c2fbc665746e3ee65ca4f96
jihwooon <jihwooon@gmail.com>
2024-08-27 00:27:17 +0900

    Next 설정 및 API 호출 코드 수정
    Next 설정에 로깅 설정을 추가하고, API 호출 코드에 revalidate 옵션을 추가하였습니다.
    
    주요 변경사항:
    1. next.config.mjs 파일에 fetches 로깅 설정 추가: fetch 요청의 전체 URL을 로깅하도록 설정하였습니다.
    2. RecoBooks 함수에서 API 호출 시 revalidate 옵션을 추가: 이로써, 해당 API 호출 결과는 3초 동안 유효하게 됩니다.

--- onebit-next/section04/src/app/(with-searchbar)/page.tsx
+++ onebit-next/section04/src/app/(with-searchbar)/page.tsx
@@ -21,7 +21,9 @@ export async function AllBooks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_KEY_SERVER_URL}/book/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_KEY_SERVER_URL}/book/random`, {
+    next: { revalidate: 3 }
+  });
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>
   }
