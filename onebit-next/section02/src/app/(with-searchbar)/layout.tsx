dbae6f1192fbc4c4b49d0ff53c6e7fa84b57735f
jihwooon <jihwooon@gmail.com>
2024-09-01 21:28:09 +0900

    검색바 컴포넌트 추가 및 레이아웃에 적용
    검색 기능을 위한 검색바 컴포넌트를 추가하고, 해당 컴포넌트를 레이아웃에 적용하였습니다.
    
    주요 변경사항:
    1. 검색바 컴포넌트(Searchbar)를 새로 생성하였습니다. 이 컴포넌트는 입력값을 상태로 관리하며, 입력값이 변경될 때마다 상태를 업데이트합니다.
    2. 레이아웃 컴포넌트(SearchLayout)에 검색바 컴포넌트를 추가하였습니다. 이로써 사용자는 레이아웃에서 검색 기능을 사용할 수 있게 되었습니다.

--- onebit-next/section02/src/app/(with-searchbar)/layout.tsx
+++ onebit-next/section02/src/app/(with-searchbar)/layout.tsx
@@ -1,3 +1,5 @@
+import Searchbar from '@/app/(with-searchbar)/searchbar';
+
 interface Props {
   children: React.ReactNode;
 }
@@ -6,6 +8,7 @@ export default function SearchLayout({ children }: Props) {
   return (
     <div>
       <div>서치바</div>
+      <Searchbar />
       <div>{children}</div>
     </div>
   );
