04432ef603e122dafe6f48758d18e2a66f9724df
jihwooon <jihwooon@gmail.com>
2024-09-06 21:49:59 +0900

    리뷰 생성 기능 추가 및 CSS 수정
    리뷰 생성 기능을 분리하고, 해당 기능을 사용하는 컴포넌트에 적용하였습니다. 또한 CSS의 일부를 수정하였습니다.
    
    주요 변경사항:
    1. 'create-review.action.ts' 파일을 새로 생성하여 리뷰 생성 기능을 분리하였습니다.
    2. 'page.tsx'에서 'createReviewAction' 함수를 분리하고, 'create-review.action.ts'에서 가져와 사용하도록 변경하였습니다.
    3. 'page.tsx'의 'ReviewEditor' 컴포넌트에 'bookId' prop을 추가하였습니다.
    4. 'page.module.css'에서 '.container > section'의 CSS를 수정하였습니다.

---
+++ onebit-next/section06/src/actions/create-review.action.ts
@@ -0,0 +1,21 @@
+'use server';
+
+export async function createReviewAction(formData: FormData) {
+  const content = formData.get('content')?.toString();
+  const author = formData.get('author')?.toString();
+  const bookId = formData.get('bookId')?.toString();
+
+  if (!content || !author || !bookId) {
+    return;
+  }
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
+      method: 'POST',
+      body: JSON.stringify({ bookId, content, author }),
+    });
+    console.log(response.status);
+  } catch (err) {
+    console.error(err);
+  }
+}
--- onebit-next/section06/src/app/book/[id]/page.tsx
+++ onebit-next/section06/src/app/book/[id]/page.tsx
@@ -1,24 +1,18 @@
 import { BookDetail } from '@/app/book/[id]/BookDetail';
 import style from '@/app/book/[id]/page.module.css';
+import { createReviewAction } from '@/actions/create-review.action';
 
 export function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
 
-function ReviewEditor() {
-  async function createReviewAction(formData: FormData) {
-    'use server';
-    const content = formData.get('content');
-    const author = formData.get('author');
-
-    console.log(content, author);
-  }
-
+function ReviewEditor({ bookId }: { bookId: string }) {
   return (
     <section>
       <form action={createReviewAction}>
-        <input name="content" placeholder="리뷰 내용" />
-        <input name="author" placeholder="작성자" />
+        <input name="bookId" value={bookId} hidden />
+        <input required name="content" placeholder="리뷰 내용" />
+        <input required name="author" placeholder="작성자" />
         <button type="submit">작성하기</button>
       </form>
     </section>
@@ -29,7 +23,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <div className={style.container}>
       <BookDetail bookId={params.id} />
-      <ReviewEditor />
+      <ReviewEditor bookId={params.id} />
     </div>
   );
 }
