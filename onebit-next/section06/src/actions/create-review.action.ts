59ba2bd31c15a75203a6431f420305d3856629d8
jihwooon <jihwooon@gmail.com>
2024-09-07 16:48:41 +0900

    리뷰 생성 기능 개선 및 에러 처리 추가
    리뷰 생성 기능에 대한 에러 처리를 추가하고, 사용자 경험을 개선하기 위해 로딩 상태를 표시하도록 변경하였습니다.
    
    주요 변경사항:
    1. createReviewAction 함수에 에러 처리 로직 추가: 리뷰 내용이나 작성자가 없을 경우, 서버 응답이 정상이 아닐 경우 에러 메시지를 반환하도록 변경하였습니다.
    2. createReviewAction 함수에 delay 함수를 사용하여 2초 지연을 추가하였습니다.
    3. ReviewEditor 컴포넌트에 useActionState와 useEffect를 사용하여 액션의 상태를 관리하고, 에러 메시지를 표시하도록 변경하였습니다.
    4. 리뷰 작성 중인 경우 버튼을 비활성화하고 로딩 상태를 표시하도록 변경하였습니다.

--- onebit-next/section06/src/actions/create-review.action.ts
+++ onebit-next/section06/src/actions/create-review.action.ts
@@ -1,31 +1,40 @@
 'use server';
 
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidateTag } from 'next/cache';
+import { delay } from '@/util/delay';
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
   const bookId = formData.get('bookId')?.toString();
 
   if (!content || !author || !bookId) {
-    return;
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요.',
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
       method: 'POST',
       body: JSON.stringify({ bookId, content, author }),
     });
-    // revalidatePath(`/book/${bookId}`);
-    //
-    // revalidatePath('/book/[id]', 'page');
-    //
-    // revalidatePath('/(with-searchbar)', 'layout');
-    //
-    // revalidatePath('/', 'layout');
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: '',
+    };
   } catch (err) {
-    console.error(err);
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
