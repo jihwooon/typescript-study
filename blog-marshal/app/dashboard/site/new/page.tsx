71ea6dc25b16411352b15ed21396a48f19c0c693
jihwooon <jihwooon@gmail.com>
2024-11-11 15:02:33 +0900

    blog-marshal 환경설정을 하라

--- blog-marshal/app/dashboard/site/new/page.tsx
+++ blog-marshal/app/dashboard/site/new/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { CreateSiteAction } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Label } from "@radix-ui/react-dropdown-menu";
+import { useActionState } from "react";
+import { useForm } from '@conform-to/react'
+import { parseWithZod } from "@conform-to/zod";
+import { siteSchema } from "@/app/utils/zodSchemas";
+import { Label } from "@/components/ui/label";
 
 export default function NewSiteRoute () {
+    const [lastResult, action] = useActionState(CreateSiteAction, undefined);
+   const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+        return parseWithZod(formData,{
+            schema: siteSchema
+        } )
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput"
+   })
+
     return (
         <div className="flex flex-col flex-1 items-center justify-center">
             <Card className="max-w-[450px]">
@@ -14,11 +35,19 @@ export default function NewSiteRoute () {
                     Create your here. Click the button below once your done...
                   </CardDescription>
                 </CardHeader> 
+                <form id={form.id} onSubmit={form.onSubmit} action={action}>
                 <CardContent>
                     <div className="flex flex-col gap-y-6">
                         <div className="grid gap-2">
                             <Label>Site Name</Label>
-                            <Input placeholder="Site Name" suppressHydrationWarning/>
+                            <Input 
+                            name={fields.name.name} 
+                            key={fields.name.key} 
+                            defaultValue={fields.name.initialValue} 
+                            placeholder="Site Name" 
+                            suppressHydrationWarning
+                            />
+                            <p className="text-red-500 text-sm">{fields.name.errors}</p>
                         </div> 
                         <div className="gap-2">
                             <Label>Subdirectory</Label>
@@ -33,6 +62,7 @@ export default function NewSiteRoute () {
                 <CardFooter>
                     <Button suppressHydrationWarning>Submit</Button>
                 </CardFooter>
+                </form>
             </Card>                        
         </div>
     )
