fe743c85e15b918ae31dddf78b1d825a0c13cc4d
jihwooon <jihwooon@gmail.com>
2024-11-11 16:34:23 +0900

    라우트 그룹 폴더를 생성하라

---
+++ z-come/src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function home () {
+    return <main><h1>홈페이지</h1></main>
+}
--- z-come/src/app/home/Layout.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-    children: React.ReactNode
-}
-
-export default function RootLayout({children}: Props) {
-    return <div>{children}</div>
-}
