a53439530e6e120e5561e0fe9caa0b1e114c75b8
jihwooon <jihwooon@gmail.com>
2024-11-11 17:11:55 +0900

    Link, Image, redirect를 적용하라

---
+++ z-come/src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+    return <div><h1>로그인 성공!</h1></div>
+}
--- z-come/src/app/page.tsx
+++ z-come/src/app/page.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import zLogo from "../../public/zlogo.png"
 
 export default function Home() {
-  return <div>
-    <h1>Home</h1>
-  </div>
+  return(
+      <div>
+        <div>
+            <Image src={zLogo}alt="logo" />
+        </div>
+        <div>
+          <h1>지금 일어나고 있는 일</h1>
+          <h1>지금 가입하세요.</h1>
+          <Link href="/i/flow/signup">계정 만들기</Link>
+          <h3>트위터에 가입하셨나요?</h3>
+            <Link href="/login">로그인</Link>
+        </div>
+      </div>
+  )
 }
