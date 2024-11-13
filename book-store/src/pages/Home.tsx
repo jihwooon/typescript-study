3d6c7bcb121799c79c08d991851cbc8b87e69e55
jihwooon <jihwooon@gmail.com>
2024-11-13 13:11:38 +0900

    프로그래머스 환경설정

---
+++ book-store/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/common/Header";
+import {formatNumber} from "../utils/format";
+
+const COUNT = 100;
+
+function Home() {
+    return <>
+        <Header />
+        <div>book store</div>
+        <div>count: {formatNumber(COUNT)}</div>
+    </>
+}
+
+export default Home;
