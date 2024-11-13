dbfaf1216d209007c0ba1bee1605f19112554a5c
jihwooon <jihwooon@gmail.com>
2024-11-13 23:11:32 +0900

    Button 컴포넌트를 추가하라

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Title from "../components/common/Title";
+import Button from "../components/common/Button";
 
 function Home() {
     return <>
         <Title size="medium" color="background">제목 테스트</Title>
         <div>home body</div>
+        <Button size="large" scheme="primary" disabled={true}>버튼 테스트</Button>
     </>
 }
 
