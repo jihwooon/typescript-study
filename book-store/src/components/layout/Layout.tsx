d476583a66f65a9e1ca32419aff9fefcc54e406e
jihwooon <jihwooon@gmail.com>
2024-11-14 10:57:53 +0900

    헤더와 푸터를 적용하라

--- book-store/src/components/layout/Layout.tsx
+++ book-store/src/components/layout/Layout.tsx
@@ -1,16 +1,26 @@
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import {styled} from "styled-components";
 
 interface LayoutProps {
     children: React.ReactNode
 }
 
 function Layout({children}: LayoutProps) {
-    return <>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
-    </>
+    return(
+        <>
+            <Header/>
+            <LayoutStyle>{children}</LayoutStyle>
+            <Footer/>
+        </>
+    )
 }
+
+const LayoutStyle = styled.main`
+    width: 100%;
+    margin: 0 auto;
+    max-width: ${({theme}) => theme.layout.width.layout};
+    padding: 20px 0;
+`;
 
 export default Layout;
