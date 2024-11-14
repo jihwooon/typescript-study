71443d633bd097116abb4330bbc21453a0f331ba
jihwooon <jihwooon@gmail.com>
2024-11-14 15:56:00 +0900

    api 통신과 데이터 레이러를 하라

--- book-store/src/components/common/Header.tsx
+++ book-store/src/components/common/Header.tsx
@@ -1,27 +1,11 @@
 import {styled} from "styled-components";
 import {FaRegUser, FaSignInAlt} from "react-icons/fa";
 import {Link} from "react-router-dom";
-
-const CATEGORY = [
-    {
-        id: null,
-        name: "전체"
-    },
-    {
-        id: 0,
-        name: "동화"
-    },
-    {
-        id: 1,
-        name: "소셜"
-    },
-    {
-        id: 2,
-        name: "사회"
-    }
-]
+import {useCategory} from "../../hook/useCategory";
 
 function Header() {
+    const { category } = useCategory();
+
     return (
         <HeaderStyle>
             <h1 className="logo">
@@ -32,7 +16,7 @@ function Header() {
             <nav className="category">
             <ul>
                     {
-                        CATEGORY.map((item) => (
+                        category.map((item) => (
                             <li key={item.id}>
                                 <Link to={item.id == null ? '/books' : `/books?category_id=${item.id}`}>
                                     {item.name}
