f4b736f2f607346910436d75ee3103366c0034d2
jihwooon <jihwooon@gmail.com>
2024-11-27 13:13:08 +0900

    메인 화면 리뷰 세션을 추가하라

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,14 +1,20 @@
-import Title from "../components/common/Title";
-import Button from "../components/common/Button";
-import InputText from "../components/common/InputText";
+import {useMain} from "../hook/useMain";
+import {styled} from "styled-components";
+import MainReview from "../components/home/MainReview";
 
 function Home() {
-    return <>
-        <Title size="medium" color="background">제목 테스트</Title>
-        <div>home body</div>
-        <Button size="large" scheme="primary" disabled={true}>버튼 테스트</Button>
-        <InputText placeholder="여기에 입력해주세요"></InputText>
-    </>
+    const { reviews } = useMain();
+    return (
+        <HomeStyle>
+            {/* 배너 */}
+            {/* 베스트셀러 */}
+            {/* 신가 */}
+            {/* 리뷰 */}
+            <MainReview reviews={reviews}/>
+        </HomeStyle>
+    )
 }
+
+const HomeStyle = styled.div``
 
 export default Home;
