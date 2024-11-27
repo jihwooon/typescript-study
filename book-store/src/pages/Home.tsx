2cf226eefd375f1fd14f7c8aaa2ed86d41dab08d
jihwooon <jihwooon@gmail.com>
2024-11-27 14:00:52 +0900

    메인 시간 세션을 추가하라

--- book-store/src/pages/Home.tsx
+++ book-store/src/pages/Home.tsx
@@ -1,16 +1,28 @@
 import {useMain} from "../hook/useMain";
 import {styled} from "styled-components";
 import MainReview from "../components/home/MainReview";
+import Title from "../components/common/Title";
+import MainNewBooks from "../components/home/MainNewBooks";
 
 function Home() {
-    const { reviews } = useMain();
+    const { reviews, newBooks } = useMain();
     return (
         <HomeStyle>
             {/* 배너 */}
             {/* 베스트셀러 */}
+            <section className="section">
+                <Title size="large">베스트 셀러</Title>
+            </section>
             {/* 신가 */}
+            <section className="section">
+                <Title size="large">신간 안내</Title>
+                <MainNewBooks books={newBooks} />
+            </section>
             {/* 리뷰 */}
-            <MainReview reviews={reviews}/>
+            <section className="section">
+                <Title size="large">리뷰</Title>
+                <MainReview reviews={reviews}/>
+            </section>
         </HomeStyle>
     )
 }
