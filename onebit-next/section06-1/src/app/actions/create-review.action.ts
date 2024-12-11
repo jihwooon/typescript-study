dd2cab6343a90b3b7af3c2a2bc5ba48e229e549e
jihwooon <jihwooon@gmail.com>
2024-12-11 18:35:03 +0900

    한입 북스 - 클라이언트 컴포넌트에서 서버 액선 실행하기

--- onebit-next/section06-1/src/app/actions/create-review.action.ts
+++ onebit-next/section06-1/src/app/actions/create-review.action.ts
@@ -1,17 +1,22 @@
 'use server'
 
 import { revalidatePath } from 'next/cache'
+import {delay} from "@/util/delay";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
     const bookId= formData.get("bookId")?.toString();
     const content = formData.get('content')?.toString();
     const author = formData.get('author')?.toString();
 
     if (!(bookId && content && author)) {
-        return;
+        return {
+            status: false,
+            error: "리뷰 내용과 작성자를 작성해주세요"
+        }
     }
 
     try {
+        await delay(20000);
         const response = await fetch(`http://localhost:12345/review`, {
             method: "POST",
             body: JSON.stringify({
@@ -19,9 +24,15 @@ export async function createReviewAction(formData: FormData) {
             })
         });
 
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
         revalidatePath(`review-${bookId}`);
     } catch(err) {
         console.error(err)
-        return;
+        return {
+            status: false,
+            error: "리뷰 내용과 작성자를 작성해주세요"
+        }
     }
 }
