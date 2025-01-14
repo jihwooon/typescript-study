69a2a02b30b58f4e3c912cfbfdeed9284c686db6
jihwooon <jihwooon@gmail.com>
2025-01-15 00:35:51 +0900

    유니온 타입 및 테마 인터페이스 추가
    유니온 타입과 테마 인터페이스를 추가하고, light 및 dark 테마 객체를 정의했습니다.
    
    주요 변경사항:
    1. 유니온 타입 ThemeName, Colorkey, HeadingSize, ButtonSize 추가
    2. Theme 인터페이스 정의
    3. light 테마 객체 정의
    4. dark 테마 객체 정의 및 light 테마를 기반으로 확장
    5. 불필요한 테스트 파일 삭제

--- book/pro-typescript/src/ch06/01.ts
+++ book/pro-typescript/src/ch06/01.ts
@@ -1,26 +1,53 @@
-/**
- * 인터섹션 타입
- */
+// 유니온 타입은 타입 T e U 중 하나가 될 수 있는 타입이다.
+type ThemeName = 'light' | 'dark';
+type Colorkey = 'primary' | 'background' | 'secondary' | 'third' | 'border' | 'text';
+type HeadingSize = 'large' | 'normal' | 'small'
+type ButtonSize = 'large' | 'normal' | 'small'
 
-type Admin = {
-	name: string;
-	privileges: string[]
+type ButtonKey = keyof ButtonSize
+
+interface Theme {
+	name: ThemeName;
+	color: Record<Colorkey, string>
+	heading: {
+		[key in HeadingSize]: {
+			fontSize: string
+		}
+	},
+}
+
+const light: Theme = {
+	name: 'light',
+	color: {
+		primary: "#ff5800",
+		background: "lightgrey",
+		secondary: "5F5F5F",
+		third: "green",
+		border: "grey",
+		text: "black"
+	},
+	heading: {
+		large: {
+			fontSize: '1rem'
+		},
+		normal: {
+			fontSize: '1.5rem'
+		},
+		small: {
+			fontSize: '0.5rem'
+		}
+	},
 };
 
-type Employee = {
-	name: string;
-	startDate: Date;
-}
-
-type ElevatedEmployee = Admin & Employee;
-
-const employee: ElevatedEmployee = {
-	name: 'MAX',
-	privileges: ['create-server'],
-	startDate: new Date()
-}
-
-export type Combinable = string | number;
-export type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
+const dark: Theme = {
+	...light,
+	name: 'dark',
+	color: {
+		primary: "#ff5800",
+		background: "lightgrey",
+		secondary: "5F5F5F",
+		third: "green",
+		border: "grey",
+		text: "black"
+	},
+};
