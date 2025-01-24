15d0f0cd67ec6ed8bf430aa22d37efd25cfed1f7
jihwooon <jihwooon@gmail.com>
2025-01-25 01:30:14 +0900

    유니언 타입 및 클래스 예제 코드 추가
    유니언 타입과 클래스 사용 예제 코드를 추가했습니다. 이를 통해 타입스크립트의 타입 가드와 클래스 상속을 이해할 수 있습니다.
    
    주요 변경사항:
    1. `add` 함수 추가: 두 개의 `Combinable` 타입 인자를 받아 문자열 또는 숫자를 더하는 함수.
    2. `printEmployeeInformation` 함수 추가: `UnknownEmployee` 타입 인자를 받아 권한과 시작 날짜를 출력하는 함수.
    3. `Car` 및 `Truck` 클래스 추가: 각각 `drive` 메서드를 포함하며, `Truck` 클래스는 추가로 `loadCargo` 메서드를 포함.
    4. `useVehicle` 함수 추가: `Vehicle` 타입 인자를 받아 `drive` 메서드를 호출하고, `Truck` 인스턴스일 경우 `loadCargo` 메서드도 호출.

--- udemy/advanced-typescript/adv-types-01-starting-setup/src/app.ts
+++ udemy/advanced-typescript/adv-types-01-starting-setup/src/app.ts
@@ -21,6 +21,60 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // number 타입
 
+const add = (a: Combinable, b: Combinable) => {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployee) => {
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation({ name: 'Max', privileges: ['create-server'], startDate: new Date() });
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo...' + amount);
+    }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
 type StringAndNumber = string & number; // never 타입
 
 type Animal = {
