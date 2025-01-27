f70acae9fbdf6a85ad577d6b2cf6aeb4518f2a9d
jihwooon <jihwooon@gmail.com>
2025-01-27 14:18:57 +0900

    비밀번호 검증 기능 추가 및 테스트 코드 작성
    비밀번호 검증 기능을 추가하고, 해당 기능에 대한 테스트 코드를 작성했습니다.
    
    주요 변경사항:
    1. `password-verifier0.js` 파일에 `verifyPassword` 함수 추가
    2. `verifyPassword` 함수는 입력된 비밀번호와 규칙 리스트를 받아 검증하고, 실패한 규칙의 이유를 반환
    3. `password-verifier0.spec.js` 파일에 `verifyPassword` 함수에 대한 테스트 코드 작성
    4. 테스트는 규칙이 실패할 경우 올바른 오류 메시지를 반환하는지 확인

---
+++ book/the-art-of-unit-testing/ch2-first-test/password-verifier0.js
@@ -0,0 +1,15 @@
+const verifyPassword = (input, rules) => {
+  
+    const errors = [];
+    rules.forEach((rule) => {
+      const result = rule(input);
+      if (!result.passed) {
+        errors.push(`error ${result.reason}`);
+      }
+    });
+    return errors;
+  };
+  
+  module.exports = {
+    verifyPassword,
+  };
