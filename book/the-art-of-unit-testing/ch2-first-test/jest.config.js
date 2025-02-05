a7095d149babcbed7677a9aa3f92905c863eb6ef
jihwooon <jihwooon@gmail.com>
2025-02-05 13:19:47 +0900

    제목: Jest 설정 파일에서 결과 파일 경로 제거
    Jest 설정 파일에서 테스트 결과 파일 경로 설정을 제거했습니다.
    이로 인해 테스트 결과 파일이 생성되지 않습니다.
    
    주요 변경사항:
    1. `jest.config.js` 파일에서 `filePath` 설정 제거
    2. 테스트 결과 파일 경로 설정 제거로 인해 테스트 결과 파일이 생성되지 않음

--- book/the-art-of-unit-testing/ch2-first-test/jest.config.js
+++ book/the-art-of-unit-testing/ch2-first-test/jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
         "jest-tap-reporter",
         {
           logLevel: "ERROR", // 로그 레벨 (ERROR, WARN, INFO)
-          filePath: "test-results.tap", // 결과 파일 경로
           showInternalStackTraces: true, // 내부 스택 트레이스 표시 여부
         }
       ]
