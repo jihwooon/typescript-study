948de0bc949b826592bd579579addbd0546697f8
jihwooon <jihwooon@gmail.com>
2025-02-08 14:11:48 +0900

    버튼 색상 변경 기능 추가 및 테스트 코드 수정
    버튼 클릭 시 색상이 변경되는 기능을 추가하고, 이에 대한 테스트 코드를 수정했습니다.
    
    주요 변경사항:
    1. `App.tsx` 파일에 버튼 색상 변경 기능 추가
    2. `App.css` 파일에 버튼 색상 클래스 추가 (`red`, `blue`)
    3. `App.test.tsx` 파일에 버튼 색상 변경 테스트 코드 추가
    4. `setupTests.js` 파일에 `@testing-library/jest-dom` 임포트 추가

--- udemy/testing-library/vite-starter/src/App.test.tsx
+++ udemy/testing-library/vite-starter/src/App.test.tsx
@@ -1,9 +1,39 @@
-import { render, screen } from '@testing-library/react'
+import { logRoles, render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
-test('App contains correct heading', () => {
-    render(<App />)
-    const headingElement = screen.getByText(/learn react/i);
+// test('button starts with correct color', () => {
+//     const { container } = render(<App />)
+//     logRoles(container)
 
-    expect(headingElement).toBeInTheDocument();
-});
+//     const buttonElement = screen.getByRole('button', { name: /red/i}) 
+//     expect(buttonElement).toHaveClass('red')
+// });
+
+// test('button stats with correct text', () => {
+//     const { container } = render(<App />)
+
+//     logRoles(container)
+
+//     const buttonElement = screen.getByRole('button', {name :/blue/i})
+     
+//     expect(buttonElement).toHaveClass('blue')
+// })
+
+test('button has correct color after click', () => {
+    const { container } = render(<App/>)
+
+    logRoles(container)
+
+    const buttoneElememnt = screen.getByRole('button', { name: /blue/i })
+
+    fireEvent.click(buttoneElememnt);
+
+    expect(buttoneElememnt).toHaveTextContent(/red/i);
+    expect(buttoneElememnt).toHaveStyle({"": ""})
+
+    expect(buttoneElememnt).toHaveClass('blue')
+})
+
+test('button has correct text after click', () => {
+
+})
--- udemy/testing-library/vite-starter/src/App.tsx
+++ udemy/testing-library/vite-starter/src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import "./App.css"
+
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const nextColor = buttonColor === 'red' ? "blue" : "red";
+
   return (
     <div>
       <h1>I'm gonna learn React Testing Library</h1>
+      <button 
+        className={buttonColor} 
+        onClick={() => setButtonColor(nextColor)
+      }
+      >Change to {nextColor}
+      </button>
     </div>
   );
 }
