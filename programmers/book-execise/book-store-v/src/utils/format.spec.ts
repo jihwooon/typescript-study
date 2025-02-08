0e7da250fde02fd796239a290dac86b1cc181270
jihwooon <jihwooon@gmail.com>
2025-02-08 23:27:23 +0900

    숫자 포맷 함수 및 테스트 추가
    숫자를 천 단위로 쉼표를 추가하여 포맷하는 함수를 추가하고, 이에 대한 테스트를 작성했습니다.
    
    주요 변경사항:
    1. formatNumber 함수 추가: 숫자를 천 단위로 쉼표를 추가하여 포맷하는 함수입니다.
    2. format.spec.ts 파일 추가: formatNumber 함수에 대한 테스트 케이스를 포함합니다.
       - 천 단위 숫자 포맷 테스트
       - 백만 단위 숫자 포맷 테스트
       - 억 단위 숫자 포맷 테스트
       - 조 단위 숫자 포맷 테스트

---
+++ programmers/book-execise/book-store-v/src/utils/format.spec.ts
@@ -0,0 +1,27 @@
+import { formatNumber } from "./format"
+
+describe('format', () => {
+    it('천 단위 숫자가 주어졌을 때 3자리씩 끊어서 쉼표로 포맷해야 한다', () => {
+        const fomatedNumber = formatNumber(1000);
+
+        expect(fomatedNumber).toBe('1,000');
+    })
+
+    it('백만 단위 숫자가 주어졌을 때 3자리씩 끊어서 쉼표로 포맷해야 한다', () => {
+        const fomatedNumber = formatNumber(1000000);
+
+        expect(fomatedNumber).toBe('1,000,000');
+    })
+
+    it('억 단위 숫자가 주어졌을 때 3자리씩 끊어서 쉼표로 포맷해야 한다', () => {
+        const fomatedNumber = formatNumber(100000000);
+
+        expect(fomatedNumber).toBe('100,000,000');
+    })
+
+    it('조 단위 숫자가 주어졌을 때 3자리씩 끊어서 쉼표로 포맷해야 한다', () => {
+        const fomatedNumber = formatNumber(1000000000000);
+
+        expect(fomatedNumber).toBe('1,000,000,000,000');
+    })
+})
