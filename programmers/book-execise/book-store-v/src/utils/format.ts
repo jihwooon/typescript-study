90526ee2483f138b169250a323ec94eba286a8d9
jihwooon <jihwooon@gmail.com>
2025-02-09 00:03:55 +0900

    .gitignore 및 package.json 수정, Vite 설정 업데이트
    .gitignore 파일에 새로운 항목 추가, package.json에 typecheck 스크립트 추가, Vite 설정 파일 수정
    
    주요 변경사항:
    1. .gitignore 파일에 playground 및 _output 디렉토리 추가
    2. package.json에 typecheck 스크립트 추가
    3. Vite 설정 파일에 build 출력 디렉토리 _output으로 설정

--- programmers/book-execise/book-store-v/src/utils/format.ts
+++ programmers/book-execise/book-store-v/src/utils/format.ts
@@ -1,3 +1,3 @@
 export const formatNumber = (number: number) => {
-    return number.toLocaleString()
+  return number.toLocaleString()
 }
--- programmers/book-execise/book-store-v/vite.config.ts
+++ programmers/book-execise/book-store-v/vite.config.ts
@@ -9,4 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
+  build: {
+    outDir: '_output',
+  },
 })
