33d374bd1b66f086da0036e1c9824b4bd590530d
jihwooon <jihwooon@gmail.com>
2025-02-08 16:56:22 +0900

    테스트 라이브러리 추가 및 App 컴포넌트 수정
    테스트 환경 설정을 위해 필요한 라이브러리를 추가하고, App 컴포넌트를 수정했습니다.
    
    주요 변경사항:
    1. @testing-library/jest-dom 및 @testing-library/react 추가
    2. jsdom 라이브러리 추가
    3. App 컴포넌트의 불필요한 코드 제거 및 텍스트 변경
    4. App 컴포넌트에 대한 테스트 파일(App.spec.tsx) 추가
    5. 테스트 환경 설정 파일(setupTests.ts) 추가
    6. tsconfig.app.json 파일에 테스트 파일 포함
    7. vite.config.ts 파일에 테스트 환경 설정 추가

---
+++ programmers/book-execise/book-store-v/src/App.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('App', () => {
+  it('컴포넌트를 렌더링하면, Vitest 텍스트가 화면에 보여야 한다.', () => {
+    render(<App />)
+        
+    const textElement = screen.getByText(/Vitest/i)
+        
+    expect(textElement).toHaveTextContent('Vitest Test!');
+  })
+})
