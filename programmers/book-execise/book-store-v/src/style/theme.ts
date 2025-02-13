cd95e3715ea295cc52cceca5d2ea3941315a780a
jihwooon <jihwooon@gmail.com>
2025-02-14 00:36:10 +0900

    버튼 컴포넌트 추가 및 테마 업데이트
    버튼 컴포넌트를 추가하고 테마 파일을 업데이트했습니다.
    
    주요 변경사항:
    1. `Button.tsx` 파일 추가: 버튼 컴포넌트 구현
    2. `Home.tsx` 파일 수정: 버튼 컴포넌트 사용
    3. `theme.ts` 파일 수정: 버튼 크기와 색상 테마 추가 및 업데이트

--- programmers/book-execise/book-store-v/src/style/theme.ts
+++ programmers/book-execise/book-store-v/src/style/theme.ts
@@ -1,6 +1,8 @@
 export type ThemeName = 'light' | 'dark'
 export type Colorkey = 'primary' | 'background' | 'secondary' | 'third'
 export type HeadingSize = "large" | "medium" | "small"
+export type ButtonSize = "small" | "medium" | "large"
+export type ButtonScheme = "primary" | "secondary" | "nomal"
 
 interface Theme {
     name: ThemeName;
@@ -9,6 +11,21 @@ interface Theme {
       [key in HeadingSize]: {
         fontSize: string;
       }
+    }
+    button: {
+      [key in ButtonSize]: {
+        fontSize: string;
+        padding: string;
+      }
+    },
+    buttonScheme: {
+      [key in ButtonScheme]: {
+        color: string;
+        background: string;
+      }
+    },
+    borderRadius: {
+      default: string;
     }
 }
 
@@ -30,6 +47,37 @@ export const light: Theme = {
     small: {
       fontSize: "1rem",
     },
+  },
+  button: {
+    small: {
+      fontSize: "0.8rem",
+      padding: "0.5rem 1rem",
+    },
+    medium: {
+      fontSize: "1rem",
+      padding: "0.7rem 1.5rem",
+    },
+    large: {
+      fontSize: "1.5rem",
+      padding: "1rem 2rem",
+    },
+  },
+  buttonScheme: {
+    primary: {
+      color: "white",
+      background: "midnightblue",
+    },
+    secondary: {
+      color: "white",
+      background: "#5F5F5F",
+    },
+    nomal: {
+      color: "black",
+      background: "white",
+    },
+  },
+  borderRadius: {
+    default: "4px",
   },
 }
 
