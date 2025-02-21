e71b8f2eefbd93e84bd84496f8e0d3e0b771eceb
jihwooon <jihwooon@gmail.com>
2025-02-22 00:48:16 +0900

    마우스 감도 따라가기 구현하기

--- dream-coding/basic-xy/src/AppXY.jsx
+++ dream-coding/basic-xy/src/AppXY.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handlePointerMove = (event) => {
+    // 모든 방향으로 포인터가 이동아 기능하다면?
+    setPosition({x: event.x, y: event.clientY})
+    // 수평으로 이동이 가능하다면?
+    // setPosition(prev => ({x: event.clientX, y: prev.y}))
+    // 수직만 이동이 가능하다면?
+    setPosition(prev => ({x: prev.x, y: event.clientY}))
+  };
+
   return (
-    <div className='container'>
-      <div className='pointer' />
+    <div className="container" onPointerMove={handlePointerMove}>
+      <div
+        className="pointer"
+        style={{ left: position.x, top: position.y }}
+      />
     </div>
   );
 }
