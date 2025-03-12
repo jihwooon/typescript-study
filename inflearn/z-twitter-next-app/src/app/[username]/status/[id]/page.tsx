c98bbba709d5509b35f3fa1bf3126620981cb499
jihwooon <jihwooon@gmail.com>
2025-03-12 21:58:29 +0900

    페이지 컴포넌트 추가 및 패키지 업데이트
    새로운 페이지 컴포넌트를 추가하고 패키지 버전을 업데이트했습니다.
    
    주요 변경사항:
    1. 로그인, 유저 페이지, 유저 상세페이지, 탐색 페이지, 홈 페이지, 검색 페이지, 404 페이지 컴포넌트 추가
    2. package.json의 next, react, react-dom, @eslint/eslintrc, typescript 패키지 버전 업데이트
    3. tsconfig.json에 새로운 경로 추가

---
+++ inflearn/z-twitter-next-app/src/app/[username]/status/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>유저 상세페이지</h1>
+        </div>
+    );
+}
+
+export default Page;
---
+++ inflearn/z-twitter-next-app/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>홈 페이지</h1>
+        </div>
+    );
+}
+
+export default Page;
