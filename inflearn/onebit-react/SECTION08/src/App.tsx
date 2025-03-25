4e957b93401d3420ba94630775dba780851ec9af
jihwooon <jihwooon@gmail.com>
2025-03-26 00:17:14 +0900

    사용자 입력 필드 추가 및 짝수 컴포넌트 구현
    사용자 입력 필드를 추가하고, 짝수일 때 표시되는 컴포넌트를 구현했습니다. 또한, useEffect 훅을 사용하여 컴포넌트의 마운트 및 업데이트 시 로그를 출력하도록 했습니다.
    
    주요 변경사항:
    1. `useEffect`, `useRef` 훅 추가
    2. 사용자 입력 필드 추가 및 상태 관리
    3. 짝수일 때 표시되는 `Even` 컴포넌트 구현
    4. 컴포넌트 마운트 및 업데이트 시 콘솔 로그 출력

--- inflearn/onebit-react/SECTION08/src/App.tsx
+++ inflearn/onebit-react/SECTION08/src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Viewer from './componets/Viewer';
 import Controller from './componets/Controller';
+import Even from './componets/Even';
 
 function App() {
   const [count, setCount] = useState<number>(0)
+  const [input, setInput] = useState<string>("");
+
+  const isMount = useRef(false)
+
+  useEffect(() => {
+    console.log(`${count} ${input}`)
+  }, [count, input])
+
+  // useEffect(() => {
+  //   console.log("mount");
+  // }, [])
+
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+
+    console.log('update');
+  })
 
   const handleClick = (value: number) => {
     setCount(count + value);
@@ -14,7 +35,13 @@ function App() {
     <div>
       <h1>Simple Counter</h1>
       <section>
+        <input type="text" value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInput(e.target.value)
+        }}/>
+      </section>
+      <section>
         <Viewer count={count}/>
+        {count % 2 === 0 ? <Even/> : null}
       </section>
       <section>
         <div className="counter-button">
