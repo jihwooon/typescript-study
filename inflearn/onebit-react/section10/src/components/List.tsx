234bcadf6771c0c3f1d1aa26a84a1bde7adaf6f2
jihwooon <jihwooon@gmail.com>
2025-03-29 10:22:44 +0900

    검색 기능 추가 및 Todo 리스트 필터링
    Todo 리스트에 검색 기능을 추가하여 사용자가 입력한 검색어에 따라 리스트를 필터링할 수 있도록 수정했습니다.
    
    주요 변경사항:
    1. useState를 사용하여 검색어 상태 관리 추가
    2. onChangeSearch 함수 추가로 입력된 검색어 상태 업데이트
    3. getFilteredData 함수 추가로 검색어에 따라 필터링된 Todo 리스트 반환
    4. 필터링된 Todo 리스트를 렌더링하도록 수정

--- inflearn/onebit-react/section10/src/components/List.tsx
+++ inflearn/onebit-react/section10/src/components/List.tsx
@@ -1,23 +1,47 @@
 import './List.css'
 import { TodoItem as ITodos } from '../models/todo-item.model'
 import { TodoItem } from './TodoItem'
+import { useState } from 'react'
 
 interface Props {
     todos: ITodos[]
 }
 
 export const List = ({ todos }: Props) => {
-    return (
-        <div className="List">
-            <h4>Todo List</h4>
-            <input placeholder="검색어를 입력하세요." />
-            <div className='todos_wrapper'>  
-             {
-                todos.map((item) => (
-                    <TodoItem todo={item}/>
-                ))
-             }
-            </div>
-        </div>
-    )
+  const [search, setSearch] =useState("")
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  const getFilteredData = () => {
+    if(search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) =>
+      todo.content
+      .toLowerCase()
+      .includes(search.toLowerCase())
+    );
+  }
+
+  const filteredTodos = getFilteredData()
+
+  return (
+    <div className="List">
+      <h4>Todo List</h4>
+      <input
+        type="text"
+        value={search}
+        onChange={onChangeSearch}
+        placeholder='검색어를 입력하세요.'
+      />
+      <div className='todos_wrapper'>
+        {filteredTodos.map((item) => (
+          <TodoItem key={item.id} todo={item}/>
+        ))}
+      </div>
+    </div>
+  )
 }
