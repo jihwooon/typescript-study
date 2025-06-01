e4da7b2c21ad27b835fe45b3fddb949b6683f651
jihwooon <jihwooon@gmail.com>
2025-06-01 15:55:03 +0900

    상품 목록 페이지 추가 및 라우터 수정
    상품 목록 페이지를 추가하고, 해당 페이지를 라우터에 연결했습니다. 또한 React Query 패키지를 설치하여 향후 데이터 관리에 활용할 준비를 했습니다.
    
    주요 변경사항:
    1. `ProductList.tsx` 파일 생성 및 상품 목록 페이지 구현.
    2. `router.tsx`에서 상품 목록 페이지를 라우터에 연결.
    3. `@tanstack/react-query` 패키지 설치 및 `yarn.lock` 업데이트.

---
+++ inflearn/shopping-mall/client/src/pages/products/ProductList.tsx
@@ -0,0 +1,9 @@
+const ProductList = () => {
+  return (
+    <div>
+      <h1>상품 목록</h1>
+    </div>
+  )
+}
+
+export default ProductList
--- inflearn/shopping-mall/client/src/router.tsx
+++ inflearn/shopping-mall/client/src/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router"
 import Layout from "./pages/layout"
+import ProductList from "./pages/products/ProductList"
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route index element={<div>Home</div>} />
-      <Route path="products" element={<div>Products</div>} />
+      <Route path="products" element={<ProductList />} />
       <Route path="products/:productId" element={<div>Product Detail</div>} />
     </Route>
   )
