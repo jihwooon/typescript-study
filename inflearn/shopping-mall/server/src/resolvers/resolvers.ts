62a9c0fd768aa7bfe3accfde5eeadd904bf84f8b
jihwooon <jihwooon@gmail.com>
2025-06-06 16:03:02 +0900

    resolvers를 나눠라

--- inflearn/shopping-mall/server/src/resolvers/resolvers.ts
+++ inflearn/shopping-mall/server/src/resolvers/resolvers.ts
@@ -1,3 +1,6 @@
+import { productResolvers } from './product';
+import { cartResolvers } from './cart';
+
 const mockProducts = (() => Array.from({ length: 20 }, (_, index) => ({
   id: index + 1 + '',
   createdAt: new Date().toISOString(),
@@ -12,47 +15,10 @@ let cartData: { [key: string]: any } = {};
 
 export const resolvers = {
   Query: {
-    products: () => mockProducts,
-    product: (_: any, { id }: { id: string }) => {
-      return mockProducts.find((product) => product.id === id);
-    },
-    cart: () => Object.values(cartData),
+    ...productResolvers.Query,
+    ...cartResolvers.Query,
   },
   Mutation: {
-    addCart: (_: any, { id }: { id: string }) => {
-      const targetProduct = mockProducts.find((product) => product.id === id);
-      if (!targetProduct) {
-        throw new Error('상품이 없습니다.');
-      }
-
-      const newItem = {
-        ...targetProduct,
-        amount: (cartData[id]?.amount || 0) + 1
-      };
-      cartData[id] = newItem;
-      return newItem;
-    },
-    updateCart: (_: any, { id, amount }: { id: string; amount: number }) => {
-      if (!cartData[id]) {
-        throw new Error('존재하지 않는 데이터입니다.');
-      }
-
-      const newItem = {
-        ...cartData[id],
-        amount
-      };
-      cartData[id] = newItem;
-      return newItem;
-    },
-    deleteCart: (_: any, { id }: { id: string }) => {
-      delete cartData[id];
-      return id;
-    },
-    executePay: (_: any, { ids }: { ids: string[] }) => {
-      ids.forEach((id) => {
-        delete cartData[id];
-      });
-      return ids;
-    },
+    ...cartResolvers.Mutation,
   },
 }; 
