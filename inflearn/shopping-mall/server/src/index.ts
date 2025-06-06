1d28b83b62a1a60bd8d0209c481ddd67cba8f7d3
jihwooon <jihwooon@gmail.com>
2025-06-06 09:32:58 +0900

    server 환경 구성을 하라

---
+++ inflearn/shopping-mall/server/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+
+(async () => {
+  const server = new ApolloServer(null);
+  const port = 3000
+
+  const app = express()
+  await server.start()
+
+  // CORS 설정 옵션
+  const corsOptions = {
+    origin: ['http://localhost:3000'], // 허용할 도메인
+    credentials: true, // 쿠키 포함 요청 허용
+  };
+
+  // 전역 CORS 설정
+  app.use(cors(corsOptions));
+
+  // 특정 경로에 대한 CORS 설정
+  app.use('/graphql',
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true
+    }),
+    express.json(),
+  );
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+})()
