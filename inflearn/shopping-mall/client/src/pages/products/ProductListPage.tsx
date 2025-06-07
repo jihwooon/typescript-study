8f274dbecb37707617590a48d60a95f6ce4fd580
jihwooon <jihwooon@gmail.com>
2025-06-07 23:52:22 +0900

    상품 목록 페이지에 무한 스크롤 기능 추가
    상품 목록 데이터를 페이지 단위로 처리하고, 무한 스크롤을 지원하도록 수정했습니다. GraphQL 쿼리 및 React Query를 활용하여 데이터를 효율적으로 가져오도록 구현했습니다.
    
    주요 변경사항:
    1. `ProductList` 컴포넌트의 데이터 구조를 페이지 단위로 변경하여 `list` prop을 수정.
    2. GraphQL `GET_PRODUCTS` 쿼리에 `cursor` 파라미터를 추가하여 페이지 단위 데이터 요청 지원.
    3. `ProductListPage`에서 `useQuery` 대신 `useInfiniteQuery`를 사용하여 무한 스크롤 기능 구현.
    4. `getNextPageParam`을 통해 다음 페이지의 커서 값을 계산하도록 설정.

--- inflearn/shopping-mall/client/src/pages/products/ProductListPage.tsx
+++ inflearn/shopping-mall/client/src/pages/products/ProductListPage.tsx
@@ -1,13 +1,20 @@
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 import { graphqlFetcher, QueryKeys } from "../../queryClient"
 import { GET_PRODUCTS, type Products } from "../../graphql/products"
 import ProductList from "../../compoents/products/ProductList"
 
 const ProductListPage: React.FC = () => {
-  const { data, isLoading, error } = useQuery<Products>({
+  const { data, isLoading, error } = useInfiniteQuery<Products>({
     queryKey: [QueryKeys.PRODUCTS],
-    queryFn: () => graphqlFetcher(GET_PRODUCTS),
+    queryFn: ({ pageParam = '' }) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      console.log(lastPage, allPages)
+      return lastPage.products.at(-1)?.id
+    }
   })
+
+  console.log(data)
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
@@ -15,7 +22,7 @@ const ProductListPage: React.FC = () => {
   return (
     <div>
       <h3>상품 목록</h3> 
-      <ProductList list={data?.products || []}/>
+      <ProductList list={data?.pages || []}/>
     </div>
   )
 }
