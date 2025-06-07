29ad8d50d3efa76987df5666720a0a86c4651ddc
jihwooon <jihwooon@gmail.com>
2025-06-08 00:32:43 +0900

    상품 목록 페이지에 무한 스크롤 기능 추가
    상품 목록 페이지에 IntersectionObserver를 활용한 무한 스크롤 기능을 구현했습니다. 이를 통해 사용자가 페이지 하단에 도달하면 자동으로 추가 데이터를 로드할 수 있습니다.
    
    주요 변경사항:
    1. IntersectionObserver를 활용하여 fetchMoreRef가 화면에 보이는지 감지하는 로직 추가.
    2. useInfiniteQuery의 getNextPageParam 로직 수정으로 다음 페이지 데이터를 가져오는 방식 개선.
    3. 상품 목록 하단에 데이터를 로드하기 위한 div 요소(fetchMoreRef) 추가.

--- inflearn/shopping-mall/client/src/pages/products/ProductListPage.tsx
+++ inflearn/shopping-mall/client/src/pages/products/ProductListPage.tsx
@@ -2,19 +2,38 @@ import { useInfiniteQuery } from "@tanstack/react-query"
 import { graphqlFetcher, QueryKeys } from "../../queryClient"
 import { GET_PRODUCTS, type Products } from "../../graphql/products"
 import ProductList from "../../compoents/products/ProductList"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 const ProductListPage: React.FC = () => {
+  const observerRef = useRef<IntersectionObserver>()
+  const fetchMoreRef = useRef<HTMLDivElement>(null)
+  const [intersercting, setIntersercting] = useState(false)
+
+  const getObserver = useCallback(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver((entries) => {
+        setIntersercting(entries[0]?.isIntersecting)
+      })
+    }
+    return observerRef.current
+  }, [observerRef.current])
+
+  console.log(intersercting)
+
+  useEffect(() => {
+    if(fetchMoreRef.current) {
+      getObserver().observe(fetchMoreRef.current)
+    }
+  }, [fetchMoreRef.current])
+
   const { data, isLoading, error } = useInfiniteQuery<Products>({
     queryKey: [QueryKeys.PRODUCTS],
     queryFn: ({ pageParam = '' }) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
     initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      console.log(lastPage, allPages)
+    getNextPageParam: (lastPage) => {
       return lastPage.products.at(-1)?.id
     }
   })
-
-  console.log(data)
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
@@ -23,6 +42,7 @@ const ProductListPage: React.FC = () => {
     <div>
       <h3>상품 목록</h3> 
       <ProductList list={data?.pages || []}/>
+      <div ref={fetchMoreRef}/>
     </div>
   )
 }
