cdd58d93e9206ace2853286923aae3c36e6449ad
jihwooon <jihwooon@gmail.com>
2025-06-12 20:51:33 +0900

    Player 컴포넌트에 사용자 이름 입력 및 표시 기능 추가
    Player 컴포넌트에 사용자 이름을 입력하고 화면에 표시할 수 있는 기능을 추가했습니다.
    입력된 이름은 버튼 클릭 시 화면에 반영됩니다.
    
    주요 변경사항:
    1. React의 useState를 사용하여 사용자 이름 상태 관리 추가.
    2. 입력 필드에 onChange 핸들러를 연결하여 사용자 입력값을 상태에 저장.
    3. 버튼 클릭 시 상태를 업데이트하여 입력된 이름을 화면에 표시.
    4. 기본 메시지 "unknown entity"를 입력값이 없을 경우 표시하도록 수정.
    5. package.json에 yarn 패키지 매니저 정보 추가.

--- udemy/the-complete-guide-to-react/01-starting-project 2/src/components/Player.jsx
+++ udemy/the-complete-guide-to-react/01-starting-project 2/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submiited, setSubmitted] = useState(false);
+
+  const handleChage = (event) => {
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome
+        {submiited ? enteredPlayerName : 'unknown entity'}
+      </h2>
       <p>
-        <input type="text" />
-        <button type="button">Set Name</button>
+        <input type="text" onChange={handleChage} value={enteredPlayerName} />
+        <button type="button" onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
