5b7ccdd9a4dfbacb319225d73f7bf314f5242416
jihwooon <jihwooon@gmail.com>
2026-03-03 21:27:27 +0700

    플렉스 레이아웃 및 보더 스타일 예제 추가
    App 컴포넌트에 플렉스 컨테이너와 다양한 보더 스타일 예제를 추가했습니다. 일부 Tailwind CSS 클래스의 순서를 조정하여 가독성과 일관성을 높였습니다.
    
    주요 변경사항:
    1. 플렉스 컨테이너 예제 및 내부 아이템(1~4번 박스) 추가
    2. 다양한 보더 스타일 및 색상 예제 추가
    3. Tailwind CSS 클래스 순서 및 마진(m-5) 위치 수정

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -10,10 +10,22 @@ function App() {
       <div className="bg-amber-50">amber-500</div>
 
       {/*3/ 사이즈 */}
-      <div className="w-full h-20 bg-blue-500">box</div> 
+      <div className="h-20 w-full bg-blue-500">box</div>
 
-      <div className="h-50 w-50 bg-red-400 pt-5 pr-5 pb-5 pl-5 m-5">
+      <div className="m-5 h-50 w-50 bg-red-400 pt-5 pr-5 pb-5 pl-5">
         <div className="h-full w-full bg-blue-400"></div>
+      </div>
+
+      {/* 5. 보더 */}
+      <div className="m-5 border-x border-y-2 border-r-2">border</div>
+      <div className="rounded-md border-2 border-red-100">border color</div>
+
+      {/* 6. 플렉스 컨테이너 */}
+      <div className="flex flex-row items-start justify-evenly">
+        <div className="h-10 w-10 border">1</div>
+        <div className="h-20 w-10 flex-1 border">2</div>
+        <div className="h-30 w-10 border">3</div>
+        <div className="h-40 w-10 border">4</div>
       </div>
     </div>
   )
