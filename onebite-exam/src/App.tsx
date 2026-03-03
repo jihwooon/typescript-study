79f0191e91ab6f77ae36e7b63410c68a47d2517b
jihwooon <jihwooon@gmail.com>
2026-03-03 21:13:07 +0700

    타입스크립트 App 컴포넌트에 Tailwind 스타일 예제 추가
    App 컴포넌트에 다양한 Tailwind CSS 텍스트, 배경, 사이즈, 마진/패딩 예제를 추가했습니다. 스타일별로 구분된 div를 통해 시각적으로 확인할 수 있도록 구성했습니다.
    
    주요 변경사항:
    1. 텍스트 크기 예제(div) 추가: text-8xl, text-2xl, text-[13px] 등
    2. 배경색 예제(div) 추가: bg-amber-50, bg-blue-500, bg-red-400 등
    3. 사이즈 및 여백 예제(div) 추가: w-full, h-20, h-50, w-50, pt-5, pr-5, pb-5, pl-5, m-5 등
    4. 중첩 div를 활용한 배경색 및 크기 조합 예제 추가

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -6,6 +6,15 @@ function App() {
       <div className="text-8xl">text-8xl</div>
       <div className="text-2xl">text-2xl</div>
       <div className="text-[13px]">text-13</div>
+      {/* 2. 백그라운드 컬러 */}
+      <div className="bg-amber-50">amber-500</div>
+
+      {/*3/ 사이즈 */}
+      <div className="w-full h-20 bg-blue-500">box</div> 
+
+      <div className="h-50 w-50 bg-red-400 pt-5 pr-5 pb-5 pl-5 m-5">
+        <div className="h-full w-full bg-blue-400"></div>
+      </div>
     </div>
   )
 }
