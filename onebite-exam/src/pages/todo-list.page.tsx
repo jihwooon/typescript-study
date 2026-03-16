6e0a9f29bcc0e562c7807e2ec5dbd8e8589a86da
jihwooon <jihwooon@gmail.com>
2026-03-16 23:21:22 +0700

    Todo 리스트 상태 관리 로직 추가 및 컴포넌트 연동
    Todo 리스트를 zustand로 상태 관리하도록 변경하였습니다. 할 일 추가/삭제 기능을 구현하고, 관련 컴포넌트와 타입 정의를 연동하였습니다.
    
    주요 변경사항:
    1. zustand와 immer를 활용한 todo 상태 관리 store(onebite-exam/src/store/todos.ts) 추가
    2. 할 일 추가/삭제 기능을 TodoEditor, TodoItem 컴포넌트에 연동
    3. Todo 타입 정의(onebite-exam/src/tyoes.ts) 추가 및 적용
    4. 기존 dummy 데이터 사용을 store 기반으로 변경
    5. 오타 수정 및 불필요한 코드 제거

--- onebite-exam/src/pages/todo-list.page.tsx
+++ onebite-exam/src/pages/todo-list.page.tsx
@@ -1,5 +1,6 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
+import { useTodso } from "@/store/todos";
 
 const dummyTodo = [
   { id: 1, content: "할 일 1" },
@@ -9,12 +10,14 @@ const dummyTodo = [
 
 
 export default function TodoListPage() {
+  const todos = useTodso()
+
   return (
     <div className="p-5 flex flex-col gap-5">
       <h1 className="text-2xl font-bold">Todo List</h1>
       <TodoEditor />
       {
-        dummyTodo.map((todo) => (
+        todos.map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))
       }
